eff490154983eceb21c82f1c40a1acc664912674
Naz <bluspringindohq@gmail.com>
2023-01-23 06:44:59 +0800

    pls work

--- src/main/java/xyz/bluspring/kilt/forgeinjects/server/packs/repository/PackRepositoryInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/server/packs/repository/PackRepositoryInject.java
@@ -8,12 +8,12 @@ import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.fml.ModLoader;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.bluspring.kilt.injections.server.PackRepositoryInjection;
-import xyz.bluspring.kilt.mixin.PackRepositoryAccessor;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -21,11 +21,12 @@ import java.util.Set;
 
 @Mixin(PackRepository.class)
 public class PackRepositoryInject implements PackRepositoryInjection {
-    @Shadow @Final private Set<RepositorySource> sources;
+    @Shadow @Final @Mutable
+    private Set<RepositorySource> sources;
 
     @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/server/packs/repository/Pack$PackConstructor;[Lnet/minecraft/server/packs/repository/RepositorySource;)V")
     public void kilt$makeSourcesMutable(Pack.PackConstructor packConstructor, RepositorySource[] repositorySources, CallbackInfo ci) {
-        ((PackRepositoryAccessor) this).setSources(new HashSet<>(Arrays.asList(repositorySources)));
+        this.sources = new HashSet<>(Arrays.asList(repositorySources));
     }
 
     @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/server/packs/PackType;[Lnet/minecraft/server/packs/repository/RepositorySource;)V")
