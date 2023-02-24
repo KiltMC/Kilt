c02c3630ae570b46df81f821131f8deb715f4029
Naz <bluspringindohq@gmail.com>
2023-02-25 05:59:28 +0800

    I didn't realize some mods actually used this

--- src/main/kotlin/net/minecraftforge/fml/javafmlmod/FMLModContainer.kt
+++ src/main/kotlin/net/minecraftforge/fml/javafmlmod/FMLModContainer.kt
@@ -1,7 +1,12 @@
 package net.minecraftforge.fml.javafmlmod
 
+import net.minecraftforge.eventbus.api.IEventBus
 import net.minecraftforge.fml.ModContainer
 import net.minecraftforge.forgespi.language.IModInfo
 
-class FMLModContainer(info: IModInfo) : ModContainer(info) {
+open class FMLModContainer(info: IModInfo) : ModContainer(info) {
+    open val eventBus: IEventBus?
+        get() {
+            return null
+        }
 }
--- src/main/kotlin/xyz/bluspring/kilt/loader/KiltModContainer.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/KiltModContainer.kt
@@ -1,10 +1,11 @@
 package xyz.bluspring.kilt.loader
 
-import net.minecraftforge.fml.ModContainer
+import net.minecraftforge.eventbus.api.IEventBus
+import net.minecraftforge.fml.javafmlmod.FMLModContainer
 import java.util.*
 import java.util.function.Consumer
 
-class KiltModContainer(mod: ForgeMod) : ModContainer(mod.forgeSpi) {
+class KiltModContainer(private val mod: ForgeMod) : FMLModContainer(mod.forgeSpi) {
     val fabricModContainer = KiltFabricModContainer(mod)
 
     init {
@@ -12,4 +13,7 @@ class KiltModContainer(mod: ForgeMod) : ModContainer(mod.forgeSpi) {
             mod.eventBus.post(it.self())
         })
     }
+
+    override val eventBus: IEventBus
+        get() = mod.eventBus
 }
