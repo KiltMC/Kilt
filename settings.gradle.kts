af23ada81013aa1a93d1a7af73e3cafbbcc27c07
Naz <bluspringindohq@gmail.com>
2023-02-26 11:39:13 +0800

    Start using the Kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    val loom_version: String by settings
+    val fabric_kotlin_version: String by settings
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version
+                fabric_kotlin_version
+                    .split("+kotlin.")[1] // Grabs the sentence after `+kotlin.`
+                    .split("+")[0] // Ensures sentences like `+build.1` are ignored
+    }
+}
