2106e4adc45171a193c65234416e0143f5d5c937
Naz <bluspringindohq@gmail.com>
2023-03-22 21:34:49 +0800

    well fuck

--- src/main/kotlin/net/minecraftforge/fml/loading/JarVersionLookupHandler.kt
+++ src/main/kotlin/net/minecraftforge/fml/loading/JarVersionLookupHandler.kt
@@ -1,8 +1,12 @@
 package net.minecraftforge.fml.loading
 
+import net.minecraftforge.versions.forge.ForgeVersion
+import xyz.bluspring.kilt.loader.KiltLoader
 import java.util.Optional
 
 object JarVersionLookupHandler {
+    private val forgeVersionClass = ForgeVersion::class.java
+
     @JvmStatic
     fun getImplementationVersion(clazz: Class<*>): Optional<String> {
         return Optional.empty()
@@ -10,6 +14,9 @@ object JarVersionLookupHandler {
 
     @JvmStatic
     fun getSpecificationVersion(clazz: Class<*>): Optional<String> {
+        if (clazz == forgeVersionClass)
+            return Optional.of(KiltLoader.SUPPORTED_FORGE_SPEC_VERSION.toString())
+
         return Optional.empty()
     }
 
