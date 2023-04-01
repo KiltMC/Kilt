528e4972916b1acb05f1a197a7759ace101b8838
Naz <bluspringindohq@gmail.com>
2023-04-01 23:53:43 +0800

    honestly there may still be something broken

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/model/BlockModelInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/model/BlockModelInject.java
@@ -1,8 +1,16 @@
 package xyz.bluspring.kilt.forgeinjects.client.renderer.block.model;
 
+import com.google.gson.Gson;
 import net.minecraft.client.renderer.block.model.BlockModel;
+import net.minecraftforge.client.model.ExtendedBlockModelDeserializer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(BlockModel.class)
 public class BlockModelInject {
+    @ModifyArg(method = "fromStream", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/GsonHelper;fromJson(Lcom/google/gson/Gson;Ljava/io/Reader;Ljava/lang/Class;)Ljava/lang/Object;"))
+    private static Gson kilt$useForgeExtendedBlockModelDeserializer(Gson gson) {
+        return ExtendedBlockModelDeserializer.INSTANCE;
+    }
 }
