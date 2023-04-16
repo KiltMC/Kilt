9c549d094e65ea50ced536a610b27db2b0be76da
Naz <bluspringindohq@gmail.com>
2023-04-16 14:42:44 +0800

    I think it's safe to add these now

--- src/main/java/xyz/bluspring/kilt/forgeinjects/server/network/ServerLoginPacketListenerImplInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/server/network/ServerLoginPacketListenerImplInject.java
@@ -2,12 +2,17 @@ package xyz.bluspring.kilt.forgeinjects.server.network;
 
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.login.ServerboundCustomQueryPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerLoginPacketListenerImpl;
+import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLoginPacketListenerImpl.class)
 public abstract class ServerLoginPacketListenerImplInject {
@@ -21,6 +26,13 @@ public abstract class ServerLoginPacketListenerImplInject {
     @Shadow private int tick;
 
     @Shadow public abstract void disconnect(Component component);
+
+    @Inject(method = "handleCustomQueryPacket", at = @At("HEAD"), cancellable = true)
+    public void kilt$handleCustomQuery(ServerboundCustomQueryPacket packet, CallbackInfo ci) {
+        if (NetworkHooks.onCustomPayload(packet, this.connection)) {
+            ci.cancel();
+        }
+    }
 
     @Mixin(targets = "net/minecraft/server/network/ServerLoginPacketListenerImpl$1")
     public static class ThreadInject {
