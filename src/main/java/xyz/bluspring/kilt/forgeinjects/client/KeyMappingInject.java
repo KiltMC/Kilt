3b1d9c4f7f83f5dd0913c3b47ebef0cbf5d4fefd
Naz <bluspringindohq@gmail.com>
2023-04-22 18:52:45 +0800

    Add Forge's constructors to KeyMappingInject

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/KeyMappingInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/KeyMappingInject.java
@@ -9,14 +9,25 @@ import net.minecraftforge.client.settings.KeyModifier;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import xyz.bluspring.kilt.helpers.mixin.CreateInitializer;
 
 import java.util.Map;
+import java.util.Set;
 
 @Mixin(KeyMapping.class)
 public abstract class KeyMappingInject implements IForgeKeyMapping {
     @Shadow private InputConstants.Key key;
     @Shadow @Final private static Map<InputConstants.Key, KeyMapping> MAP;
+    @Mutable
+    @Shadow @Final private String name;
+    @Mutable
+    @Shadow @Final private InputConstants.Key defaultKey;
+    @Mutable
+    @Shadow @Final private String category;
+    @Shadow @Final private static Map<String, KeyMapping> ALL;
+    @Shadow @Final private static Set<String> CATEGORIES;
     private KeyModifier keyModifierDefault = KeyModifier.NONE;
     private KeyModifier keyModifier = KeyModifier.NONE;
     private IKeyConflictContext keyConflictContext = KeyConflictContext.UNIVERSAL;
@@ -56,5 +67,37 @@ public abstract class KeyMappingInject implements IForgeKeyMapping {
 
         this.keyModifier = keyModifier;
         MAP.put(keyCode, (KeyMapping) (Object) this);
+    }
+
+    @CreateInitializer
+    public KeyMappingInject(String description, IKeyConflictContext keyConflictContext, final InputConstants.Type inputType, final int keyCode, String category) {
+        this(description, keyConflictContext, inputType.getOrCreate(keyCode), category);
+    }
+
+    @CreateInitializer
+    public KeyMappingInject(String description, IKeyConflictContext keyConflictContext, InputConstants.Key keyCode, String category) {
+        this(description, keyConflictContext, KeyModifier.NONE, keyCode, category);
+    }
+
+    @CreateInitializer
+    public KeyMappingInject(String description, IKeyConflictContext keyConflictContext, KeyModifier keyModifier, final InputConstants.Type inputType, final int keyCode, String category) {
+        this(description, keyConflictContext, keyModifier, inputType.getOrCreate(keyCode), category);
+    }
+
+    @CreateInitializer
+    public KeyMappingInject(String description, IKeyConflictContext keyConflictContext, KeyModifier keyModifier, InputConstants.Key keyCode, String category) {
+        this.name = description;
+        this.key = keyCode;
+        this.defaultKey = keyCode;
+        this.category = category;
+        this.keyConflictContext = keyConflictContext;
+        this.keyModifier = keyModifier;
+        this.keyModifierDefault = keyModifier;
+        if (this.keyModifier.matches(keyCode))
+            this.keyModifier = KeyModifier.NONE;
+
+        ALL.put(description, (KeyMapping) (Object) this);
+        MAP.put(keyCode, (KeyMapping) (Object) this);
+        CATEGORIES.add(category);
     }
 }
