beaba415399215f002e2cbc929cd9cbaed079f7b
Naz <bluspringindohq@gmail.com>
2023-05-27 03:21:11 +0800

    fixed modded rails

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/entity/vehicle/AbstractMinecartInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/entity/vehicle/AbstractMinecartInject.java
@@ -1,15 +1,20 @@
 package xyz.bluspring.kilt.forgeinjects.world.entity.vehicle;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.IMinecartCollisionHandler;
 import net.minecraftforge.common.extensions.IForgeAbstractMinecart;
 import net.minecraftforge.common.extensions.IForgeBaseRailBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.bluspring.kilt.helpers.mixin.CreateStatic;
 import xyz.bluspring.kilt.injections.entity.AbstractMinecartInjection;
 
@@ -30,6 +35,22 @@ public abstract class AbstractMinecartInject extends Entity implements AbstractM
     @CreateStatic
     private static void registerCollisionHandler(IMinecartCollisionHandler handler) {
         COLLISIONS = handler;
+    }
+
+    @Inject(method = "push", at = @At("HEAD"), cancellable = true)
+    public void kilt$handleForgeCollision(Entity entity, CallbackInfo ci) {
+        if (getCollisionHandler() != null) {
+            getCollisionHandler().onEntityCollision((AbstractMinecart) (Object) this, entity);
+            ci.cancel();
+        }
+    }
+
+    // TODO: onMinecartPass, need to figure out how to add a mixin there
+
+    @Inject(method = "moveAlongTrack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/vehicle/AbstractMinecart;getDeltaMovement()Lnet/minecraft/world/phys/Vec3;", ordinal = 9), cancellable = true)
+    public void kilt$checkIfShouldDoRailFunctions(BlockPos pos, BlockState state, CallbackInfo ci) {
+        if (!shouldDoRailFunctions())
+            ci.cancel();
     }
 
     private boolean canUseRail = true;
