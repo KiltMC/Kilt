ed0a273364a76cfde02dc8672a096eb59aaef6fc
Naz <bluspringindohq@gmail.com>
2023-06-22 12:52:04 +0800

    Fixed Sonic Security System crash with SecurityCraft

---
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/server/level/ServerPlayerGameModeInject.java
@@ -0,0 +1,9 @@
+package xyz.bluspring.kilt.forgeinjects.server.level;
+
+import net.minecraft.server.level.ServerPlayerGameMode;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ServerPlayerGameMode.class)
+public class ServerPlayerGameModeInject {
+
+}
