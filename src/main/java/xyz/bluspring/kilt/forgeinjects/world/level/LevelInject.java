1ffe507e6c8d05e47c5999ae0d9e3399e41fb7c8
Naz <bluspringindohq@gmail.com>
2023-06-23 01:07:17 +0800

    Add block neighbour updates

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/LevelInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/LevelInject.java
@@ -1,10 +1,12 @@
 package xyz.bluspring.kilt.forgeinjects.world.level;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityDispatcher;
@@ -14,26 +16,23 @@ import net.minecraftforge.common.extensions.IForgeBlockState;
 import net.minecraftforge.common.extensions.IForgeLevel;
 import net.minecraftforge.common.util.BlockSnapshot;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.ForgeEventFactory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.bluspring.kilt.injections.CapabilityProviderInjection;
 import xyz.bluspring.kilt.injections.capabilities.LevelCapabilityProviderImpl;
 import xyz.bluspring.kilt.injections.world.level.LevelInjection;
 import xyz.bluspring.kilt.workarounds.CapabilityProviderWorkaround;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.EnumSet;
 import java.util.function.Supplier;
 
 @Mixin(Level.class)
@@ -43,6 +42,8 @@ public abstract class LevelInject implements CapabilityProviderInjection, LevelC
     @Shadow public abstract ResourceKey<Level> dimension();
 
     @Shadow public abstract BlockState getBlockState(BlockPos blockPos);
+
+    @Shadow public abstract void updateNeighbourForOutputSignal(BlockPos pos, Block block);
 
     private final CapabilityProviderWorkaround<Level> workaround = new CapabilityProviderWorkaround<>(Level.class, (Level) (Object) this);
 
@@ -98,5 +99,38 @@ public abstract class LevelInject implements CapabilityProviderInjection, LevelC
     @Override
     public void gatherCapabilities(@Nullable Supplier<ICapabilityProvider> parent) {
         workaround.invokeGatherCapabilities(parent);
+    }
+
+    @Redirect(method = "getSignal", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isRedstoneConductor(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z"))
+    public boolean kilt$checkForWeakPower(BlockState instance, BlockGetter blockGetter, BlockPos blockPos, BlockPos unused, Direction facing) {
+        return ((IForgeBlockState) instance).shouldCheckWeakPower((Level) blockGetter, blockPos, facing);
+    }
+
+    @Redirect(method = "updateNeighbourForOutputSignal", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 0))
+    public boolean kilt$checkForNeighbourChange(BlockState instance, Block unused, BlockPos blockPos, @Local(index = 1) BlockPos directionPos) {
+        ((IForgeBlockState) instance).onNeighborChange((Level) (Object) this, directionPos, blockPos);
+        // Don't trigger the Vanilla neighbour change.
+        return false;
+    }
+
+    @Redirect(method = "updateNeighbourForOutputSignal", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 1))
+    public boolean kilt$getWeakChange(BlockState instance, Block unused, BlockPos blockPos, Block block, @Local(index = 1) BlockPos directionPos) {
+        return ((IForgeBlockState) instance).getWeakChanges((Level) (Object) this, directionPos);
+    }
+
+    @Inject(method = "blockEntityChanged", at = @At("TAIL"))
+    public void kilt$updateNeighbourOutputSignalsForChange(BlockPos pos, CallbackInfo ci) {
+        this.updateNeighbourForOutputSignal(pos, this.getBlockState(pos).getBlock());
+    }
+
+    @Inject(method = "removeBlockEntity", at = @At("TAIL"))
+    public void kilt$updateNeighbourOutputSignalsForRemoval(BlockPos pos, CallbackInfo ci) {
+        this.updateNeighbourForOutputSignal(pos, this.getBlockState(pos).getBlock());
+    }
+
+    @Inject(method = "updateNeighborsAt", at = @At("TAIL"))
+    public void kilt$notifyNeighbours(BlockPos pos, Block block, CallbackInfo ci) {
+        // why is "isCanceled()" added at the end?
+        ForgeEventFactory.onNeighborNotify((Level) (Object) this, pos, this.getBlockState(pos), EnumSet.allOf(Direction.class), false).isCanceled();
     }
 }
---
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/block/ComparatorBlockInject.java
@@ -0,0 +1,29 @@
+package xyz.bluspring.kilt.forgeinjects.world.level.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.ComparatorBlock;
+import net.minecraft.world.level.block.DiodeBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ComparatorBlock.class)
+public abstract class ComparatorBlockInject extends DiodeBlock {
+    protected ComparatorBlockInject(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public boolean getWeakChanges(BlockState state, LevelReader level, BlockPos pos) {
+        return state.is(Blocks.COMPARATOR);
+    }
+
+    @Override
+    public void onNeighborChange(BlockState state, LevelReader level, BlockPos pos, BlockPos neighbor) {
+        if (pos.getY() == neighbor.getY() && level instanceof Level && !level.isClientSide()) {
+            state.neighborChanged((Level) level, pos, level.getBlockState(neighbor).getBlock(), neighbor, false);
+        }
+    }
+}
