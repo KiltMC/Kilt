dc2fd4446b903ad7aaf5d33b3603b46d22461314
Naz <bluspringindohq@gmail.com>
2023-07-03 05:24:45 +0800

    Add static create for Rarity

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/item/RarityInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/item/RarityInject.java
@@ -1,9 +1,11 @@
 package xyz.bluspring.kilt.forgeinjects.world.item;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.common.IExtensibleEnum;
 import org.spongepowered.asm.mixin.Mixin;
+import xyz.bluspring.kilt.helpers.mixin.CreateStatic;
 import xyz.bluspring.kilt.injections.world.item.RarityInjection;
 
 import java.util.function.UnaryOperator;
@@ -20,5 +22,15 @@ public class RarityInject implements RarityInjection, IExtensibleEnum {
     @Override
     public void setStyleModifier(UnaryOperator<Style> styleModifier) {
         this.styleModifier = styleModifier;
+    }
+
+    @CreateStatic
+    private static Rarity create(String name, ChatFormatting formatting) {
+        return RarityInjection.create(name, formatting);
+    }
+
+    @CreateStatic
+    private static Rarity create(String name, UnaryOperator<Style> styleModifier) {
+        return RarityInjection.create(name, styleModifier);
     }
 }
