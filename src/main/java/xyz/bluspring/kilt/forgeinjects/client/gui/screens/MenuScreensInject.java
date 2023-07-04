a2eb52636d2a342c2b8fec5affe029784df59e0d
Naz <bluspringindohq@gmail.com>
2023-07-04 08:11:51 +0800

    Add CreateStatic for getScreenFactory

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/screens/MenuScreensInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/screens/MenuScreensInject.java
@@ -1,9 +1,20 @@
 package xyz.bluspring.kilt.forgeinjects.client.gui.screens;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
 import org.spongepowered.asm.mixin.Mixin;
+import xyz.bluspring.kilt.helpers.mixin.CreateStatic;
 import xyz.bluspring.kilt.injections.client.gui.screens.MenuScreensInjection;
+
+import java.util.Optional;
 
 @Mixin(MenuScreens.class)
 public class MenuScreensInject implements MenuScreensInjection {
+    @CreateStatic
+    private static <T extends AbstractContainerMenu> Optional<MenuScreens.ScreenConstructor<T, ?>>  getScreenFactory(MenuType<T> menuType, Minecraft minecraft, int i, Component component) {
+        return MenuScreensInjection.getScreenFactory(menuType, minecraft, i, component);
+    }
 }
