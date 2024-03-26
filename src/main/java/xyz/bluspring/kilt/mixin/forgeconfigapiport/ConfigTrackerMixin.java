0578150843c12fcaab5b88611fa2f86bac9ad5fa
Naz <bluspringindohq@gmail.com>
2024-03-26 14:32:40 +0800

    disable remap on ConfigTracker mixin

--- src/main/java/xyz/bluspring/kilt/mixin/forgeconfigapiport/ConfigTrackerMixin.java
+++ src/main/java/xyz/bluspring/kilt/mixin/forgeconfigapiport/ConfigTrackerMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ConfigTracker.class)
+@Mixin(value = ConfigTracker.class, remap = false)
 public class ConfigTrackerMixin {
     @Inject(method = "trackConfig", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Lorg/slf4j/Marker;Ljava/lang/String;[Ljava/lang/Object;)V", remap = false, shift = At.Shift.AFTER), remap = false, cancellable = true)
     private void kilt$avoidCrashOnConflict(ModConfig config, CallbackInfo ci) {
