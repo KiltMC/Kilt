ad766d3000e8010ac5243926028de860f6bd8a2c
Naz <bluspringindohq@gmail.com>
2024-04-03 03:39:05 +0800

    somewhat accomplished redirecting local vars?

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/screens/advancements/AdvancementTabInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/screens/advancements/AdvancementTabInject.java
@@ -1,6 +1,8 @@
 // TRACKED HASH: 4b65780a21b72c6641617a99b2ca8d8e31521909
 package xyz.bluspring.kilt.forgeinjects.client.gui.screens.advancements;
 
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.client.Minecraft;
@@ -12,6 +14,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import xyz.bluspring.kilt.helpers.mixin.CreateInitializer;
 import xyz.bluspring.kilt.injections.client.gui.screens.advancements.AdvancementTabInjection;
@@ -39,13 +42,18 @@ public class AdvancementTabInject implements AdvancementTabInjection {
     }
 
     @Redirect(method = "create", at = @At(value = "NEW", target = "net/minecraft/client/gui/screens/advancements/AdvancementTab"))
-    private static AdvancementTab kilt$createWithPages(Minecraft minecraft, AdvancementsScreen advancementsScreen, AdvancementTabType advancementTabType, int tabIndex, Advancement advancement, DisplayInfo displayInfo) {
-        return AdvancementTabInjection.create(minecraft, advancementsScreen, advancementTabType, tabIndex % AdvancementTabTypeInjection.MAX_TABS, tabIndex / AdvancementTabTypeInjection.MAX_TABS, advancement, displayInfo);
+    private static AdvancementTab kilt$createWithPages(Minecraft minecraft, AdvancementsScreen advancementsScreen, AdvancementTabType advancementTabType, int tabIndex, Advancement advancement, DisplayInfo displayInfo, @Share("originalTabs") LocalIntRef originalTabsValue) {
+        return AdvancementTabInjection.create(minecraft, advancementsScreen, advancementTabType, originalTabsValue.get() % AdvancementTabTypeInjection.MAX_TABS, originalTabsValue.get() / AdvancementTabTypeInjection.MAX_TABS, advancement, displayInfo);
     }
 
-    // TODO: need to figure out how to redirect local vars
-    /*@Redirect(method = "create", at = @At(value = "LOAD", opcode = Opcodes.ILOAD))
-    private static int kilt$checkWithMaxTabs(int tabs) {
+    @ModifyVariable(method = "create", at = @At(value = "LOAD", ordinal = 0), name = "tabIndex")
+    private static int kilt$checkWithMaxTabs(int tabs, @Share("originalTabs") LocalIntRef originalTabsValue) {
+        originalTabsValue.set(tabs);
+        return tabs % AdvancementTabTypeInjection.MAX_TABS;
+    }
 
-    }*/
+    @ModifyVariable(method = "create", at = @At(value = "LOAD", ordinal = 1), name = "tabIndex")
+    private static int kilt$fixMaxTabs(int tabs, @Share("originalTabs") LocalIntRef originalTabsValue) {
+        return originalTabsValue.get();
+    }
 }
