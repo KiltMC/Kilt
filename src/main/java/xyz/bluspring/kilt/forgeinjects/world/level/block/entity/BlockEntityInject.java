a6d32efacfce65b63c962a10e112efe8b157efd5
Naz <bluspringindohq@gmail.com>
2024-05-20 11:07:37 +0800

    Properly implement block entity inject

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/block/entity/BlockEntityInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/block/entity/BlockEntityInject.java
@@ -2,11 +2,17 @@
 package xyz.bluspring.kilt.forgeinjects.world.level.block.entity;
 
 import io.github.fabricators_of_create.porting_lib.extensions.extensions.BlockEntityExtensions;
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.CapabilityProvider;
 import net.minecraftforge.common.extensions.IForgeBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.bluspring.kilt.helpers.mixin.Extends;
 import xyz.bluspring.kilt.injections.CapabilityProviderInjection;
 import xyz.bluspring.kilt.injections.capabilities.BlockEntityCapabilityProviderImpl;
@@ -14,6 +20,34 @@ import xyz.bluspring.kilt.injections.capabilities.BlockEntityCapabilityProviderI
 @Mixin(BlockEntity.class)
 @Extends(CapabilityProvider.class)
 public class BlockEntityInject implements IForgeBlockEntity, CapabilityProviderInjection, BlockEntityCapabilityProviderImpl, BlockEntityExtensions {
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void kilt$gatherCapabilities(BlockEntityType<?> type, BlockPos pos, BlockState blockState, CallbackInfo ci) {
+        this.gatherCapabilities();
+    }
+
+    @Inject(method = "load", at = @At("TAIL"))
+    private void kilt$loadCapabilities(CompoundTag tag, CallbackInfo ci) {
+        if (getCapabilities() != null && tag.contains("ForgeCaps"))
+            deserializeCaps(tag.getCompound("ForgeCaps"));
+    }
+
+    @Inject(method = "saveAdditional", at = @At("TAIL"))
+    private void kilt$saveCapabilities(CompoundTag tag, CallbackInfo ci) {
+        if (getCapabilities() != null)
+            tag.put("ForgeCaps", serializeCaps());
+    }
+
+    @Inject(method = "setRemoved", at = @At("TAIL"))
+    private void kilt$updateAndInvalidateCapabilities(CallbackInfo ci) {
+        this.invalidateCaps();
+        requestModelDataUpdate();
+    }
+
+    @Override
+    public void onChunkUnloaded() {
+        this.invalidateCaps();
+    }
+
     @Override
     public CompoundTag getPersistentData() {
         return this.getCustomData();
