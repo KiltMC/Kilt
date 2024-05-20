c2533212d160df753e9e520bcc2a3740ee3f5868
Naz <bluspringindohq@gmail.com>
2024-05-20 12:13:47 +0800

    ....fixed the creative tab crash too...

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/item/ItemInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/item/ItemInject.java
@@ -5,8 +5,13 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.extensions.IForgeItem;
+import net.minecraftforge.registries.GameData;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,10 +19,13 @@ import xyz.bluspring.kilt.injections.client.renderer.RenderPropertiesInjection;
 import xyz.bluspring.kilt.injections.item.ItemInjection;
 import xyz.bluspring.kilt.injections.item.ItemPropertiesInjection;
 
+import java.util.Map;
 import java.util.function.Consumer;
 
 @Mixin(Item.class)
 public abstract class ItemInject implements IForgeItem, ItemInjection, RenderPropertiesInjection {
+    @Shadow @Final @Mutable
+    public static Map<Block, Item> BY_BLOCK;
     private boolean canRepair;
     private Object renderProperties;
 
@@ -25,6 +33,11 @@ public abstract class ItemInject implements IForgeItem, ItemInjection, RenderPro
     public void kilt$setRepairability(Item.Properties properties, CallbackInfo ci) {
         canRepair = ((ItemPropertiesInjection) properties).getCanRepair();
         kilt$initClient();
+    }
+
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void kilt$useForgeBlockItemMap(CallbackInfo ci) {
+        BY_BLOCK = GameData.getBlockItemMap();
     }
 
     private void kilt$initClient() {
