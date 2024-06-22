675f8d69d5771bd1e178ce0b48472301a8ee11ba
Naz <bluspringindohq@gmail.com>
2024-06-22 18:58:49 +0800

    Mark getRecipeBookCategories as client-only

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/inventory/RecipeBookMenuInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/inventory/RecipeBookMenuInject.java
@@ -1,6 +1,8 @@
 // TRACKED HASH: 5e2075b943a16e07d906de887d9ca451cc35cc0b
 package xyz.bluspring.kilt.forgeinjects.world.inventory;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.RecipeBookCategories;
 import net.minecraft.world.inventory.RecipeBookMenu;
 import net.minecraft.world.inventory.RecipeBookType;
@@ -14,6 +16,7 @@ import java.util.List;
 public abstract class RecipeBookMenuInject implements RecipeBookMenuInjection {
     @Shadow public abstract RecipeBookType getRecipeBookType();
 
+    @Environment(EnvType.CLIENT)
     @Override
     public List<RecipeBookCategories> getRecipeBookCategories() {
         return RecipeBookCategories.getCategories(this.getRecipeBookType());
