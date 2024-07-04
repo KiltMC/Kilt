b5354e1d8f8a7d695871e02ede149ec80382862e
Naz <bluspringindohq@gmail.com>
2024-07-04 08:02:31 +0800

    Add FMLLoader#getDist

--- src/main/kotlin/net/minecraftforge/fml/loading/FMLLoader.kt
+++ src/main/kotlin/net/minecraftforge/fml/loading/FMLLoader.kt
@@ -1,11 +1,18 @@
 package net.minecraftforge.fml.loading
 
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.fml.loading.targets.CommonLaunchHandler
 import net.minecraftforge.fml.loading.targets.KnotLaunchHandler
+import xyz.bluspring.kilt.util.DistUtil
 import java.nio.file.Path
 
 object FMLLoader {
+    @JvmStatic
+    fun getDist(): Dist {
+        return DistUtil.envTypeToDist(FabricLoader.getInstance().environmentType)
+    }
+
     @JvmStatic
     fun isProduction(): Boolean {
         return !FabricLoader.getInstance().isDevelopmentEnvironment
