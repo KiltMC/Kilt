8260760df84173b085588754dce7da17b930296b
Naz <bluspringindohq@gmail.com>
2024-08-08 02:06:27 +0800

    Fix dynamic registries

--- src/main/java/xyz/bluspring/kilt/forgeinjects/server/WorldLoaderInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/server/WorldLoaderInject.java
@@ -3,12 +3,10 @@ package xyz.bluspring.kilt.forgeinjects.server;
 
 import net.minecraft.server.WorldLoader;
 import org.spongepowered.asm.mixin.Mixin;
-import xyz.bluspring.kilt.mixin.compat.fabric_api.DynamicRegistriesMixin;
 
 @Mixin(WorldLoader.class)
 public class WorldLoaderInject {
     /**
-     * The inject for this is here,
-     * See: {@link DynamicRegistriesMixin}
+     * This is already handled by Fabric API.
      */
 }
