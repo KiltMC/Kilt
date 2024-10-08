695e95a8698e0b634f239803e2c3f903382cea89
Naz <bluspringindohq@gmail.com>
2024-10-09 03:16:42 +0800

    Use WrapOperation instead of Redirect for custom BE item renderer

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/entity/ItemRendererInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/entity/ItemRendererInject.java
@@ -1,6 +1,8 @@
 // TRACKED HASH: 709f6c8bf847a0de3a9f176c6e8340ac7efe60ac
 package xyz.bluspring.kilt.forgeinjects.client.renderer.entity;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -12,15 +14,21 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemRenderer.class)
 public class ItemRendererInject {
     @Shadow @Final private BlockEntityWithoutLevelRenderer blockEntityRenderer;
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/BlockEntityWithoutLevelRenderer;renderByItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V"))
-    public void kilt$useCustomBlockEntityRenderer(BlockEntityWithoutLevelRenderer instance, ItemStack stack, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay) {
-        var renderer = IClientItemExtensions.of(stack).getCustomRenderer();
+    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/BlockEntityWithoutLevelRenderer;renderByItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V"))
+    public void kilt$useCustomBlockEntityRenderer(BlockEntityWithoutLevelRenderer instance, ItemStack stack, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay, Operation<Void> original) {
+        var extension = IClientItemExtensions.of(stack);
+
+        if (extension == IClientItemExtensions.DEFAULT) {
+            original.call(instance, stack, displayContext, poseStack, buffer, packedLight, packedOverlay);
+            return;
+        }
+
+        var renderer = extension.getCustomRenderer();
         renderer.renderByItem(stack, displayContext, poseStack, buffer, packedLight, packedOverlay);
     }
 
