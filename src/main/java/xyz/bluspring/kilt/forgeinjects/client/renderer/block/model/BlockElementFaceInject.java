c9700fdaaacdfdd7b1c41797ecb6c9b66de93575
Naz <bluspringindohq@gmail.com>
2024-10-09 01:01:49 +0800

    Finish implementing block element injects

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/model/BlockElementFaceInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/model/BlockElementFaceInject.java
@@ -1,6 +1,10 @@
 // TRACKED HASH: c651e93bacb2243f18c5b16ca365e199444ccfb3
 package xyz.bluspring.kilt.forgeinjects.client.renderer.block.model;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.renderer.block.model.BlockElement;
 import net.minecraft.client.renderer.block.model.BlockElementFace;
 import net.minecraft.client.renderer.block.model.BlockFaceUV;
@@ -9,9 +13,14 @@ import net.minecraftforge.client.model.ForgeFaceData;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.bluspring.kilt.helpers.mixin.CreateInitializer;
 import xyz.bluspring.kilt.injections.client.renderer.block.model.BlockElementFaceInjection;
 import xyz.bluspring.kilt.injections.client.renderer.block.model.BlockElementInjection;
+
+import java.lang.reflect.Type;
 
 @Mixin(BlockElementFace.class)
 public class BlockElementFaceInject implements BlockElementFaceInjection {
@@ -43,5 +52,16 @@ public class BlockElementFaceInject implements BlockElementFaceInjection {
     @Override
     public void kilt$setFaceData(ForgeFaceData faceData) {
         this.faceData = faceData;
+    }
+
+    @Mixin(BlockElementFace.Deserializer.class)
+    public static class DeserializerInject {
+        @Inject(method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lnet/minecraft/client/renderer/block/model/BlockElementFace;", at = @At("RETURN"))
+        private void kilt$readForgeFaceData(JsonElement json, Type type, JsonDeserializationContext context, CallbackInfoReturnable<BlockElementFace> cir, @Local JsonObject jsonObject) {
+            var face = cir.getReturnValue();
+            var faceData = ForgeFaceData.read(jsonObject.get("forge_data"), null);
+
+            ((BlockElementFaceInjection) face).kilt$setFaceData(faceData);
+        }
     }
 }
--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/model/BlockElementInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/model/BlockElementInject.java
@@ -1,6 +1,10 @@
 // TRACKED HASH: 84f534b4bbc58f38de24da827b479b0d0083faaf
 package xyz.bluspring.kilt.forgeinjects.client.renderer.block.model;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.renderer.block.model.BlockElement;
 import net.minecraft.client.renderer.block.model.BlockElementFace;
 import net.minecraft.client.renderer.block.model.BlockElementRotation;
@@ -12,11 +16,17 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.bluspring.kilt.helpers.mixin.CreateInitializer;
 import xyz.bluspring.kilt.injections.client.renderer.block.model.BlockElementFaceInjection;
 import xyz.bluspring.kilt.injections.client.renderer.block.model.BlockElementInjection;
 
+import java.lang.reflect.Type;
 import java.util.Map;
+import java.util.Objects;
 
 @Mixin(BlockElement.class)
 public class BlockElementInject implements BlockElementInjection {
@@ -32,6 +42,11 @@ public class BlockElementInject implements BlockElementInjection {
         this.kilt$setFaces();
     }
 
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void kilt$initFaceValues(Vector3f from, Vector3f _to, Map faces, BlockElementRotation rotation, boolean shade, CallbackInfo ci) {
+        this.kilt$setFaces();
+    }
+
     @Override
     public void kilt$setFaces() {
         this.faces.values().forEach(face -> ((BlockElementFaceInjection) face).kilt$setParent((BlockElement) (Object) this));
@@ -44,6 +59,17 @@ public class BlockElementInject implements BlockElementInjection {
 
     @Override
     public void setFaceData(ForgeFaceData faceData) {
-        this.faceData = faceData;
+        this.faceData = Objects.requireNonNull(faceData);
+    }
+
+    @Mixin(BlockElement.Deserializer.class)
+    public static class DeserializerInject {
+        @Inject(method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lnet/minecraft/client/renderer/block/model/BlockElement;", at = @At("RETURN"))
+        private void kilt$readForgeFaceData(JsonElement json, Type type, JsonDeserializationContext context, CallbackInfoReturnable<BlockElement> cir, @Local JsonObject jsonObject) {
+            var element = cir.getReturnValue();
+            var faceData = ForgeFaceData.read(jsonObject.get("forge_data"), ForgeFaceData.DEFAULT);
+
+            ((BlockElementInjection) element).setFaceData(faceData);
+        }
     }
 }
