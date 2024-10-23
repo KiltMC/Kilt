846ffc8676fc131aad0b0a881eadf1842795c7ca
Naz <bluspringindohq@gmail.com>
2024-10-23 14:34:33 +0800

    Fix mod config saving in singleplayer

--- src/main/java/xyz/bluspring/kilt/forgeinjects/server/network/ServerLoginPacketListenerImplInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/server/network/ServerLoginPacketListenerImplInject.java
@@ -1,6 +1,8 @@
 // TRACKED HASH: edbba521b66a25813967d4ed08f1ca7f80422f08
 package xyz.bluspring.kilt.forgeinjects.server.network;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.login.ServerboundCustomQueryPacket;
@@ -13,6 +15,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ServerLoginPacketListenerImpl.class, priority = 1050)
@@ -25,6 +28,8 @@ public abstract class ServerLoginPacketListenerImplInject {
 
     @Shadow public abstract void disconnect(Component component);
 
+    @Shadow ServerLoginPacketListenerImpl.State state;
+
     @Inject(method = "handleCustomQueryPacket", at = @At("HEAD"), cancellable = true)
     public void kilt$handleCustomQuery(ServerboundCustomQueryPacket packet, CallbackInfo ci) {
         if (NetworkHooks.onCustomPayload(packet, this.connection)) {
@@ -32,9 +37,32 @@ public abstract class ServerLoginPacketListenerImplInject {
         }
     }
 
+    @WrapOperation(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl;state:Lnet/minecraft/server/network/ServerLoginPacketListenerImpl$State;", ordinal = 0))
+    private ServerLoginPacketListenerImpl.State kilt$tickNegotiatingState(ServerLoginPacketListenerImpl instance, Operation<ServerLoginPacketListenerImpl.State> original) {
+        var currentState = original.call(instance);
+
+        if (currentState == ServerLoginPacketListenerImpl.State.NEGOTIATING) {
+            if (NetworkHooks.tickNegotiation((ServerLoginPacketListenerImpl) (Object) this, this.connection, this.delayedAcceptPlayer)) {
+                this.state = ServerLoginPacketListenerImpl.State.READY_TO_ACCEPT;
+            }
+        }
+
+        return currentState;
+    }
+
+    @Redirect(method = "handleHello", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl$State;READY_TO_ACCEPT:Lnet/minecraft/server/network/ServerLoginPacketListenerImpl$State;"))
+    private ServerLoginPacketListenerImpl.State kilt$useNegotiatingState() {
+        return ServerLoginPacketListenerImpl.State.NEGOTIATING;
+    }
+
     @Mixin(targets = "net/minecraft/server/network/ServerLoginPacketListenerImpl$1")
     public static class ThreadInject {
         // TODO: there's meant to be a thread group added here, but i don't know how to
         //       redirect super calls, so hope this doesn't bite me in the ass.
+
+        @Redirect(method = "run", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerLoginPacketListenerImpl$State;READY_TO_ACCEPT:Lnet/minecraft/server/network/ServerLoginPacketListenerImpl$State;"))
+        private ServerLoginPacketListenerImpl.State kilt$useNegotiatingState() {
+            return ServerLoginPacketListenerImpl.State.NEGOTIATING;
+        }
     }
 }
--- src/main/java/xyz/bluspring/kilt/mixin/compat/forgeconfigapiport/ConfigTrackerMixin.java
+++
@@ -1,8 +0,0 @@
-package xyz.bluspring.kilt.mixin.compat.forgeconfigapiport;
-
-import net.minecraftforge.fml.config.ConfigTracker;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value = ConfigTracker.class, remap = false)
-public class ConfigTrackerMixin {
-}
