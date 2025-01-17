a1a6dd870b45eab33b9ea6aa38783e8af7e8639f
Naz <bluspringindohq@gmail.com>
2025-01-17 22:32:01 +0800

    Recursively remap special mixin annotations

--- src/main/kotlin/xyz/bluspring/kilt/loader/remap/fixers/MixinSpecialAnnotationRemapper.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/remap/fixers/MixinSpecialAnnotationRemapper.kt
@@ -14,45 +14,13 @@ object MixinSpecialAnnotationRemapper {
             val annotationsToReplace = mutableMapOf<AnnotationNode, AnnotationNode>()
 
             for (annotation in method.visibleAnnotations) {
-                var shouldChange = false
                 val values = mutableListOf<Any>()
 
-                if (annotation.values == null)
+                if (!recursiveRemapAnnotation(annotation, values))
                     continue
 
-                for (value in annotation.values) {
-                    when (value) {
-                        is String -> {
-                            values.add(tryRemapString(value).apply {
-                                if (this != value)
-                                    shouldChange = true
-                            })
-                        }
-
-                        is List<*> -> {
-                            val list = mutableListOf<Any?>()
-                            value.forEach {
-                                if (it is String)
-                                    list.add(tryRemapString(it).apply {
-                                        if (this != it)
-                                            shouldChange = true
-                                    })
-                                else
-                                    list.add(it)
-                            }
-                            values.add(list)
-                        }
-
-                        else -> {
-                            values.add(value)
-                        }
-                    }
-                }
-
-                if (shouldChange) {
-                    annotationsToReplace[annotation] = AnnotationNode(Opcodes.ASM9, annotation.desc)
-                    annotationsToReplace[annotation]!!.values = values
-                }
+                annotationsToReplace[annotation] = AnnotationNode(Opcodes.ASM9, annotation.desc)
+                annotationsToReplace[annotation]!!.values = values
             }
 
             for ((old, new) in annotationsToReplace) {
@@ -60,6 +28,57 @@ object MixinSpecialAnnotationRemapper {
                 method.visibleAnnotations.add(new)
             }
         }
+    }
+
+    private fun recursiveRemapAnnotation(annotation: AnnotationNode, values: MutableList<Any>): Boolean {
+        if (annotation.values == null)
+            return false
+
+        var shouldChange = false
+
+        for (value in annotation.values) {
+            when (value) {
+                is String -> {
+                    values.add(tryRemapString(value).apply {
+                        if (this != value)
+                            shouldChange = true
+                    })
+                }
+
+                is List<*> -> {
+                    val list = mutableListOf<Any?>()
+                    value.forEach {
+                        if (it is String)
+                            list.add(tryRemapString(it).apply {
+                                if (this != it)
+                                    shouldChange = true
+                            })
+                        else
+                            list.add(it)
+                    }
+                    values.add(list)
+                }
+
+                is AnnotationNode -> {
+                    val extraValues = mutableListOf<Any>()
+
+                    if (recursiveRemapAnnotation(value, extraValues)) {
+                        shouldChange = true
+
+                        val extraNode = AnnotationNode(Opcodes.ASM9, value.desc)
+                        extraNode.values = extraValues
+
+                        values.add(extraNode)
+                    }
+                }
+
+                else -> {
+                    values.add(value)
+                }
+            }
+        }
+
+        return shouldChange
     }
 
     private fun tryRemapString(fullDescriptor: String): String {
