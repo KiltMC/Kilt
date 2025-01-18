b7a22f98473a1da285b65e5900a6888f752825d2
Naz <bluspringindohq@gmail.com>
2025-01-18 19:02:51 +0800

    Fix values crash with UseAnim

--- src/main/java/xyz/bluspring/kilt/mixin/world/item/UseAnimAccessor.java
+++ src/main/java/xyz/bluspring/kilt/mixin/world/item/UseAnimAccessor.java
@@ -2,6 +2,7 @@ package xyz.bluspring.kilt.mixin.world.item;
 
 import net.minecraft.world.item.UseAnim;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
@@ -17,6 +18,7 @@ public interface UseAnimAccessor {
         throw new IllegalStateException();
     }
 
+    @Mutable
     @Accessor("$VALUES")
     static void setValues(UseAnim[] values) {
         throw new IllegalStateException();
