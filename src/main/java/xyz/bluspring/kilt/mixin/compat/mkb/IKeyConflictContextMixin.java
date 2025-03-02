8034a25f8ce20913065e9a2ae89f911938748797
Aueara <140941996+kir-kir@users.noreply.github.com>
2025-03-02 07:59:14 -0500

    Add @Pseudo to these (#169)

--- src/main/java/xyz/bluspring/kilt/mixin/compat/mkb/IKeyConflictContextMixin.java
+++ src/main/java/xyz/bluspring/kilt/mixin/compat/mkb/IKeyConflictContextMixin.java
@@ -3,7 +3,9 @@ package xyz.bluspring.kilt.mixin.compat.mkb;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
 import net.minecraftforge.client.settings.IKeyConflictContext;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 
+@Pseudo
 @Mixin(targets = "committee.nova.mkb.api.IKeyConflictContext")
 @IfModLoaded("mkb")
 public interface IKeyConflictContextMixin extends IKeyConflictContext {
