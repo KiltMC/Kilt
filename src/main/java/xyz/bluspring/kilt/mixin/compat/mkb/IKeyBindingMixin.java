4883763b7d50442adcf0c5293dc876f1104d1945
Kiran <ckitty1212@gmail.com>
2025-03-02 06:34:03 -0500

    Add @Pseudo to these

--- src/main/java/xyz/bluspring/kilt/mixin/compat/mkb/IKeyBindingMixin.java
+++ src/main/java/xyz/bluspring/kilt/mixin/compat/mkb/IKeyBindingMixin.java
@@ -3,7 +3,9 @@ package xyz.bluspring.kilt.mixin.compat.mkb;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
 import net.minecraftforge.client.extensions.IForgeKeyMapping;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 
+@Pseudo
 @Mixin(targets = "committee.nova.mkb.api.IKeyBinding")
 @IfModLoaded("mkb")
 public interface IKeyBindingMixin extends IForgeKeyMapping {
