8d76f461e420c2bf301406481f0586b65807f6b3
Naz <bluspringindohq@gmail.com>
2025-03-11 20:24:01 +0800

    Implement Fabric fluid type handling

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/model/BakedQuadInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/model/BakedQuadInject.java
@@ -1,12 +1,23 @@
 package xyz.bluspring.kilt.forgeinjects.client.renderer.block.model;
 
 import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.core.Direction;
 import org.spongepowered.asm.mixin.Mixin;
+import xyz.bluspring.kilt.helpers.mixin.CreateInitializer;
 import xyz.bluspring.kilt.injections.client.render.model.BakedQuadInjection;
 
 @Mixin(BakedQuad.class)
 public class BakedQuadInject implements BakedQuadInjection {
     private boolean hasAmbientOcclusion;
+
+    public BakedQuadInject(int[] vertices, int tintIndex, Direction direction, TextureAtlasSprite sprite, boolean shade) {}
+
+    @CreateInitializer
+    public BakedQuadInject(int[] vertices, int tintIndex, Direction direction, TextureAtlasSprite sprite, boolean shade, boolean hasAmbientOcclusion) {
+        this(vertices, tintIndex, direction, sprite, shade);
+        this.hasAmbientOcclusion = hasAmbientOcclusion;
+    }
 
     @Override
     public void kilt$setAmbientOcclusion(boolean hasAo) {
