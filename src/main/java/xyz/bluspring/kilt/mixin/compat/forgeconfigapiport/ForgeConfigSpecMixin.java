5aca8931635c35f9050fb142787b3fe390387fc9
Naz <bluspringindohq@gmail.com>
2025-03-12 02:35:31 +0800

    shit

--- src/main/java/xyz/bluspring/kilt/mixin/compat/forgeconfigapiport/ForgeConfigSpecMixin.java
+++ src/main/java/xyz/bluspring/kilt/mixin/compat/forgeconfigapiport/ForgeConfigSpecMixin.java
@@ -5,13 +5,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ForgeConfigSpec.class)
+@Mixin(value = ForgeConfigSpec.class, remap = false)
 public abstract class ForgeConfigSpecMixin {
     @Mixin(value = ForgeConfigSpec.ConfigValue.class, remap = false)
     public static class ConfigValueMixin {
-        @Redirect(method = "get", at = @At(value = "FIELD", target = "Lnet/minecraftforge/fml/loading/FMLEnvironment;production:Z"))
-        private boolean kilt$disableDevEnvCrash() {
-            return true;
+        @Redirect(method = "get", at = @At(value = "INVOKE", target = "Lcom/google/common/base/Preconditions;checkState(ZLjava/lang/Object;)V"))
+        private void kilt$disableDevEnvCrash(boolean expression, Object errorMessage) {
         }
     }
 }
