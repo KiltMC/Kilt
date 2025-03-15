931eb67827edb4f0cc09153dbf938c39d71821a4
Naz <bluspringindohq@gmail.com>
2025-03-15 12:59:16 +0800

    adjust shadow remapping again to properly fix it

--- src/main/kotlin/xyz/bluspring/kilt/loader/remap/KiltEnhancedRemapper.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/remap/KiltEnhancedRemapper.kt
@@ -7,15 +7,15 @@ import net.minecraftforge.srgutils.IMappingFile
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
+import xyz.bluspring.kilt.util.ClassNameHashSet
 import java.util.concurrent.TimeUnit
 import java.util.function.Consumer
 
-class KiltEnhancedRemapper(provider: ClassProvider, file: IMappingFile, log: Consumer<String>) : EnhancedRemapper(provider, file, log) {
-    private val cachedLoadedClasses = CacheBuilder.newBuilder()
-        .expireAfterAccess(1L, TimeUnit.MINUTES)
-        .build<String, ClassNode>()
-
+class KiltEnhancedRemapper(provider: ClassProvider, file: IMappingFile, log: Consumer<String>, val mixinClasses: ClassNameHashSet) : EnhancedRemapper(provider, file, log) {
     override fun mapMethodName(owner: String, name: String, descriptor: String): String {
+        if (mixinClasses.contains(owner))
+            return super.mapMethodName(owner, name, descriptor)
+
         if (name.startsWith("m_") && name.endsWith("_")) {
             val mappedNames = KiltRemapper.srgMappedMethods[name] ?: return super.mapMethodName(owner, name, descriptor)
 
@@ -73,10 +73,19 @@ class KiltEnhancedRemapper(provider: ClassProvider, file: IMappingFile, log: Con
     }
 
     override fun mapFieldName(owner: String, name: String, descriptor: String): String {
+        if (mixinClasses.contains(owner))
+            return super.mapFieldName(owner, name, descriptor)
+
         if (name.startsWith("f_") && name.endsWith("_")) {
             return KiltRemapper.srgMappedFields[name]?.second ?: super.mapFieldName(owner, name, descriptor)
         }
 
         return super.mapFieldName(owner, name, descriptor)
+    }
+
+    companion object {
+        private val cachedLoadedClasses = CacheBuilder.newBuilder()
+            .expireAfterAccess(1L, TimeUnit.MINUTES)
+            .build<String, ClassNode>()
     }
 }
