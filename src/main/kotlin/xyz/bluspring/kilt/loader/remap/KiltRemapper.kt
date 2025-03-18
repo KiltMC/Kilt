e074ef33afb17c3feb9cb11ee54e65a3bf56ad3c
Naz <bluspringindohq@gmail.com>
2025-03-19 07:42:01 +0800

    fix KiltEnhancedRemapper crash in ObfuscationReflectionHelper

--- src/main/kotlin/xyz/bluspring/kilt/loader/remap/KiltRemapper.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/remap/KiltRemapper.kt
@@ -260,6 +260,9 @@ object KiltRemapper {
             ).collect { addLibrary(it) }
         }.build()
 
+        // Initialize a global remapper state
+        enhancedRemapper = KiltEnhancedRemapper(classProvider, srgIntermediaryMapping, logConsumer, ClassNameHashSet())
+
         suspend fun remapMod(file: Path, mod: ForgeMod): List<Exception> {
             val exceptions = mutableListOf<Exception>()
 
