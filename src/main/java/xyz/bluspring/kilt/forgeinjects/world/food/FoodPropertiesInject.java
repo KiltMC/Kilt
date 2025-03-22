d86061f47c289cf396830a2dbd71a0b16f047e60
Naz <bluspringindohq@gmail.com>
2025-03-22 14:03:45 +0800

    Fix Farmer's Delight effect tooltip repetition

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/food/FoodPropertiesInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/food/FoodPropertiesInject.java
@@ -3,10 +3,7 @@ package xyz.bluspring.kilt.forgeinjects.world.food;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.food.FoodProperties;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,7 +17,7 @@ import java.util.function.Supplier;
 public class FoodPropertiesInject implements FoodPropertiesInjection {
     @Shadow @Final @Mutable
     private List<Pair<MobEffectInstance, Float>> effects;
-    private List<Pair<Supplier<MobEffectInstance>, Float>> kilt$deferredEffects;
+    @Unique private List<Pair<Supplier<MobEffectInstance>, Float>> kilt$deferredEffects;
 
     @Override
     public void kilt$setDeferredEffects(List<Pair<Supplier<MobEffectInstance>, Float>> deferredEffects) {
@@ -37,6 +34,7 @@ public class FoodPropertiesInject implements FoodPropertiesInjection {
                 list.add(newPair);
             }
 
+            this.kilt$deferredEffects.clear();
             this.effects = list;
         }
     }
