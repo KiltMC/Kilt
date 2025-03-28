13b9159be97e738f09b836a5b9c283000d5c4d5f
Naz <bluspringindohq@gmail.com>
2025-03-28 08:50:56 +0800

    Hopefully fix #182 and #161

--- src/main/java/xyz/bluspring/kilt/forgeinjects/resources/RegistryDataLoaderInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/resources/RegistryDataLoaderInject.java
@@ -1,7 +1,6 @@
 package xyz.bluspring.kilt.forgeinjects.resources;
 
 import com.google.gson.JsonElement;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
@@ -13,28 +12,25 @@ import net.minecraft.core.WritableRegistry;
 import net.minecraft.resources.RegistryDataLoader;
 import net.minecraft.resources.RegistryOps;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.crafting.conditions.ICondition;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.bluspring.kilt.Kilt;
 
 import java.util.Map;
 
 @Mixin(RegistryDataLoader.class)
 public abstract class RegistryDataLoaderInject {
-    @ModifyReturnValue(method = "registryDirPath", at = @At("RETURN"))
+    /*@ModifyReturnValue(method = "registryDirPath", at = @At("RETURN")) // Kilt: Fabric handles this for us
     private static String kilt$prefixNamespaceIfForge(String original, @Local(argsOnly = true) ResourceLocation location) {
-        if (Kilt.Companion.getLoader().hasMod(location.getNamespace())) { // Normally, Forge mods are supposed to use their mod ID as the namespace.
+        if (DataPackRegistriesHooks.kilt$forgeDatapackRegistryKeys.contains(location)) {
             return ForgeHooks.prefixNamespace(location);
         }
 
         return original;
-    }
+    }*/
 
     @Inject(method = "loadRegistryContents", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Decoder;parse(Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;", shift = At.Shift.BEFORE))
     private static <E> void kilt$checkShouldRegisterEntry(RegistryOps.RegistryInfoLookup lookup, ResourceManager manager, ResourceKey<? extends Registry<E>> registryKey, WritableRegistry<E> registry, Decoder<E> decoder, Map<ResourceKey<?>, Exception> exceptions, CallbackInfo ci, @Share("shouldRegisterEntry") LocalBooleanRef shouldRegisterEntry, @Local JsonElement jsonElement) {
