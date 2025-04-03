960855547af36ade5e6dc42f67d488b20684a7d4
Naz <bluspringindohq@gmail.com>
2025-04-04 01:40:09 +0800

    Y'know, I gaslit myself into thinking this code didn't exist. and it did.

--- src/main/kotlin/xyz/bluspring/kilt/loader/KiltEarlierInitializer.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/KiltEarlierInitializer.kt
@@ -1,6 +1,5 @@
 package xyz.bluspring.kilt.loader
 
-import com.moulberry.mixinconstraints.MixinConstraints
 import de.florianmichael.asmfabricloader.api.event.PrePrePreLaunchEntrypoint
 import kotlinx.coroutines.runBlocking
 import net.fabricmc.loader.api.FabricLoader
@@ -8,11 +7,6 @@ import xyz.bluspring.kilt.loader.remap.KiltRemapper
 
 class KiltEarlierInitializer : PrePrePreLaunchEntrypoint {
     override fun onLanguageAdapterLaunch() {
-        // Ensure that MixinConstraints actually correctly uses Fabric detection.
-        val loaderField = MixinConstraints::class.java.getDeclaredField("loader")
-        loaderField.isAccessible = true
-        loaderField.set(null, MixinConstraints.Loader.FABRIC)
-
         // Force set assertions state in development mode.
         // This is useful for mods that have broken assertions, because otherwise I cannot properly
         // test Kilt in debug mode.
