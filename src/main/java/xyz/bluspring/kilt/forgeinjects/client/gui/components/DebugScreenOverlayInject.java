9b1cd5fc5e637e226b7072f7039ad325961de15a
Naz <bluspringindohq@gmail.com>
2025-04-18 05:03:13 +0800

    Improve mod compatibility with F3

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/components/DebugScreenOverlayInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/components/DebugScreenOverlayInject.java
@@ -1,14 +1,13 @@
 // TRACKED HASH: 732ba9ad75d819b941cd1599e61dbe66dbdce285
 package xyz.bluspring.kilt.forgeinjects.client.gui.components;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.DebugScreenOverlay;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
@@ -16,18 +15,17 @@ import java.util.List;
 public class DebugScreenOverlayInject {
     @Shadow @Final private Minecraft minecraft;
 
-    @Inject(method = "getSystemInformation", at = @At("RETURN"), cancellable = true)
-    public void kilt$appendEntityTags(CallbackInfoReturnable<List<String>> cir) {
+    @ModifyReturnValue(method = "getSystemInformation", at = @At("RETURN"))
+    public List<String> kilt$appendEntityTags(List<String> original) {
         if (this.minecraft.showOnlyReducedInfo())
-            return;
+            return original;
 
         var entity = this.minecraft.crosshairPickEntity;
 
         if (entity != null) {
-            var list = cir.getReturnValue();
-            entity.getType().builtInRegistryHolder().tags().forEach(t -> list.add("#" + t.location()));
-
-            cir.setReturnValue(list);
+            entity.getType().builtInRegistryHolder().tags().forEach(t -> original.add("#" + t.location()));
         }
+
+        return original;
     }
 }
