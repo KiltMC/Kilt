c14698765fc9f89c39295a84db9addd3516e1482
Naz <bluspringindohq@gmail.com>
2025-04-18 02:16:41 +0800

    Improve mod compatibility with SpawnEggItem (hopefully fixes #238)

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/item/SpawnEggItemInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/item/SpawnEggItemInject.java
@@ -3,32 +3,43 @@ package xyz.bluspring.kilt.forgeinjects.world.item;
 
 import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.SpawnEggItem;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import xyz.bluspring.kilt.injections.world.item.SpawnEggItemInjection;
+import xyz.bluspring.kilt.util.KiltHelper;
 
 @Mixin(SpawnEggItem.class)
 public class SpawnEggItemInject implements SpawnEggItemInjection {
     @Shadow @Final private EntityType<?> defaultType;
 
-    @Redirect(method = "getType", at = @At(value = "FIELD", target = "Lnet/minecraft/world/item/SpawnEggItem;defaultType:Lnet/minecraft/world/entity/EntityType;", opcode = 0))
-    private EntityType<?> kilt$useForgeDefaultType(SpawnEggItem instance) {
-        return this.getDefaultType();
+    @WrapOperation(method = "getType", at = @At(value = "FIELD", target = "Lnet/minecraft/world/item/SpawnEggItem;defaultType:Lnet/minecraft/world/entity/EntityType;", opcode = 0))
+    private EntityType<?> kilt$useForgeDefaultType(SpawnEggItem instance, Operation<EntityType<?>> original) {
+        if (KiltHelper.INSTANCE.hasMethodOverride(this.getClass(), SpawnEggItem.class, "getDefaultType"))
+            return this.getDefaultType();
+
+        return original.call(instance);
     }
 
     @ModifyReturnValue(method = "getType", at = @At("RETURN"))
     private EntityType<?> kilt$returnForgeDefaultType(EntityType<?> original) {
-        return this.getDefaultType();
+        if (KiltHelper.INSTANCE.hasMethodOverride(this.getClass(), SpawnEggItem.class, "getDefaultType"))
+            return this.getDefaultType();
+
+        return original;
     }
 
     @ModifyReceiver(method = "requiredFeatures", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/EntityType;requiredFeatures()Lnet/minecraft/world/flag/FeatureFlagSet;"))
     private EntityType<?> kilt$useForgeDefaultTypeForFeatures(EntityType<?> instance) {
-        return this.getDefaultType();
+        if (KiltHelper.INSTANCE.hasMethodOverride(this.getClass(), SpawnEggItem.class, "getDefaultType"))
+            return this.getDefaultType();
+
+        return instance;
     }
 
     @Override
