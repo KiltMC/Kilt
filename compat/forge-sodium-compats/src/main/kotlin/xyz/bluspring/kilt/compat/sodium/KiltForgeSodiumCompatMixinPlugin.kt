bcb2a39b69b55f79e3b0f66fd46574c365871501
Naz <bluspringindohq@gmail.com>
2025-04-19 09:14:57 +0800

    i hate it here

---
+++ compat/forge-sodium-compats/src/main/kotlin/xyz/bluspring/kilt/compat/sodium/KiltForgeSodiumCompatMixinPlugin.kt
@@ -0,0 +1,54 @@
+package xyz.bluspring.kilt.compat.sodium
+
+import com.moulberry.mixinconstraints.MixinConstraints
+import org.objectweb.asm.tree.ClassNode
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo
+import xyz.bluspring.kilt.loader.KiltLoader
+
+class KiltForgeSodiumCompatMixinPlugin : IMixinConfigPlugin {
+    lateinit var mixinPackage: String
+
+    override fun onLoad(mixinPackage: String) {
+        this.mixinPackage = mixinPackage
+    }
+
+    override fun getRefMapperConfig(): String? {
+        return null
+    }
+
+    override fun shouldApplyMixin(targetClassName: String?, mixinClassName: String): Boolean {
+        return try {
+            val modId = mixinClassName.removePrefix("$mixinPackage.").split(".")[0]
+            KiltLoader.INSTANCE.hasMod(modId) && MixinConstraints.shouldApplyMixin(mixinClassName)
+        } catch (_: Throwable) {
+            MixinConstraints.shouldApplyMixin(mixinClassName)
+        }
+    }
+
+    override fun acceptTargets(
+        myTargets: Set<String?>?,
+        otherTargets: Set<String?>?
+    ) {
+    }
+
+    override fun getMixins(): List<String?>? {
+        return null
+    }
+
+    override fun preApply(
+        targetClassName: String?,
+        targetClass: ClassNode?,
+        mixinClassName: String?,
+        mixinInfo: IMixinInfo?
+    ) {
+    }
+
+    override fun postApply(
+        targetClassName: String?,
+        targetClass: ClassNode?,
+        mixinClassName: String?,
+        mixinInfo: IMixinInfo?
+    ) {
+    }
+}
