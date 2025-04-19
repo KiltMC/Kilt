fa3e1f90200db0273f389ebc906eafc66892a660
Naz <bluspringindohq@gmail.com>
2025-04-19 10:18:32 +0800

    MixinConstraints why are you so good yet so awful

--- compat/forge-sodium-compats/src/main/java/xyz/bluspring/kilt/compat/sodium/mixin/immersiveengineering/SodiumWorldRendererAccessor.java
+++ compat/forge-sodium-compats/src/main/java/xyz/bluspring/kilt/compat/sodium/mixin/immersiveengineering/SodiumWorldRendererAccessor.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @IfModLoaded("sodium")
-@IfModLoaded("immersiveengineering")
 @Mixin(SodiumWorldRenderer.class)
 public interface SodiumWorldRendererAccessor {
     @Accessor
