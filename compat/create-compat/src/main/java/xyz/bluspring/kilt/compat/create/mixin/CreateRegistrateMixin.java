726a7f9bc7203d0952cb4f91cecfd557bdd12a0a
Naz <bluspringindohq@gmail.com>
2025-04-24 12:55:01 +0800

    i pray that this fixes the Create crash

--- compat/create-compat/src/main/java/xyz/bluspring/kilt/compat/create/mixin/CreateRegistrateMixin.java
+++ compat/create-compat/src/main/java/xyz/bluspring/kilt/compat/create/mixin/CreateRegistrateMixin.java
@@ -2,24 +2,28 @@ package xyz.bluspring.kilt.compat.create.mixin;
 
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.tterrag.registrate.AbstractRegistrate;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.RegistryObject;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import xyz.bluspring.kilt.compat.create.extensions.AbstractRegistrateForgeExtension;
 
 @IfModLoaded("create")
 @Mixin(CreateRegistrate.class)
-public abstract class CreateRegistrateMixin extends AbstractRegistrateMixin<CreateRegistrate> {
+public abstract class CreateRegistrateMixin extends AbstractRegistrate<CreateRegistrate> implements AbstractRegistrateForgeExtension<CreateRegistrate> {
     @Shadow @Nullable protected ResourceKey<CreativeModeTab> currentTab;
 
-    @Unique
+    protected CreateRegistrateMixin(String modid) {
+        super(modid);
+    }
+
+    /*@Unique
     public CreateRegistrate registerEventListeners(IEventBus bus) {
         return super.registerEventListeners(bus);
-    }
+    }*/
 
     public CreateRegistrate setCreativeTab(RegistryObject<CreativeModeTab> tab) {
         this.currentTab = tab.getKey();
