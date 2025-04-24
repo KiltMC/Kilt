c7469a4b8467a8f79766ac62c111c71d0dcef039
Naz <bluspringindohq@gmail.com>
2025-04-24 11:07:53 +0800

    Fix Mekanism fluid tank crash

---
+++ src/main/kotlin/xyz/bluspring/kilt/workarounds/FabricFluidTypeExtensions.kt
@@ -0,0 +1,45 @@
+package xyz.bluspring.kilt.workarounds
+
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry
+import net.minecraft.core.BlockPos
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.level.BlockAndTintGetter
+import net.minecraft.world.level.material.Fluid
+import net.minecraft.world.level.material.FluidState
+import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions
+
+class FabricFluidTypeExtensions(private val fluid: Fluid) : IClientFluidTypeExtensions {
+    private val renderHandler = FluidRenderHandlerRegistry.INSTANCE.get(fluid)
+
+    override fun getStillTexture(): ResourceLocation {
+        return renderHandler.getFluidSprites(null, null, fluid.defaultFluidState())[0].contents().name()
+    }
+
+    override fun getFlowingTexture(): ResourceLocation {
+        return renderHandler.getFluidSprites(null, null, fluid.defaultFluidState())[1].contents().name()
+    }
+
+    override fun getOverlayTexture(): ResourceLocation? {
+        return renderHandler.getFluidSprites(null, null, fluid.defaultFluidState()).getOrNull(2)?.contents()?.name()
+    }
+
+    override fun getStillTexture(state: FluidState, getter: BlockAndTintGetter?, pos: BlockPos?): ResourceLocation? {
+        return renderHandler.getFluidSprites(getter, pos, state)[0].contents().name()
+    }
+
+    override fun getFlowingTexture(state: FluidState?, getter: BlockAndTintGetter?, pos: BlockPos?): ResourceLocation? {
+        return renderHandler.getFluidSprites(getter, pos, state)[1].contents().name()
+    }
+
+    override fun getOverlayTexture(state: FluidState?, getter: BlockAndTintGetter?, pos: BlockPos?): ResourceLocation? {
+        return renderHandler.getFluidSprites(getter, pos, state).getOrNull(2)?.contents()?.name()
+    }
+
+    override fun getTintColor(): Int {
+        return renderHandler.getFluidColor(null, null, fluid.defaultFluidState())
+    }
+
+    override fun getTintColor(state: FluidState?, getter: BlockAndTintGetter?, pos: BlockPos?): Int {
+        return renderHandler.getFluidColor(getter, pos, state)
+    }
+}
