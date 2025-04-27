52889af111d7795008d86349d395add296b96b1d
Naz <bluspringindohq@gmail.com>
2025-04-27 08:41:38 +0800

    Fix GeckoLib item crash

---
+++ src/main/java/xyz/bluspring/kilt/mixin/compat/geckolib/RenderProviderMixin.java
@@ -0,0 +1,28 @@
+package xyz.bluspring.kilt.mixin.compat.geckolib;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.animatable.client.RenderProvider;
+import xyz.bluspring.kilt.compat.geckolib.ForgeRenderProvider;
+
+import java.util.function.Supplier;
+
+@Mixin(RenderProvider.class)
+public interface RenderProviderMixin {
+    @WrapOperation(method = "of(Lnet/minecraft/world/item/Item;)Lsoftware/bernie/geckolib/animatable/client/RenderProvider;", at = @At(value = "INVOKE", target = "Lsoftware/bernie/geckolib/animatable/GeoItem;getRenderProvider()Ljava/util/function/Supplier;"))
+    private static Supplier<RenderProvider> kilt$useWorkaroundRenderProvider(GeoItem instance, Operation<Supplier<RenderProvider>> original, @Local(argsOnly = true) Item item) {
+        var extensions = IClientItemExtensions.of(item);
+
+        if (extensions != IClientItemExtensions.DEFAULT) {
+            return () -> ForgeRenderProvider.Companion.get(extensions);
+        }
+
+        return original.call(instance);
+    }
+}
