38ec4e1f8cb3627c9097bf5274f8ba9600747aae
Naz <bluspringindohq@gmail.com>
2025-04-30 20:32:46 +0800

    OH COME ON

--- src/main/kotlin/xyz/bluspring/kilt/loader/mod/ForgeMod.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/mod/ForgeMod.kt
@@ -18,6 +18,7 @@ import org.apache.maven.artifact.versioning.ArtifactVersion
 import org.apache.maven.artifact.versioning.VersionRange
 import xyz.bluspring.kilt.Kilt
 import xyz.bluspring.kilt.loader.KiltModContainer
+import xyz.bluspring.kilt.loader.asm.coremod.CoreMod
 import java.io.File
 import java.io.InputStream
 import java.net.URL
