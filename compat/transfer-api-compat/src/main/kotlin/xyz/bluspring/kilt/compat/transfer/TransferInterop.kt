113e77372a1b2195223e8fdd93e2f218f9f29172
Naz <bluspringindohq@gmail.com>
2025-05-02 11:03:40 +0800

    Add item item storage capability interop

--- compat/transfer-api-compat/src/main/kotlin/xyz/bluspring/kilt/compat/transfer/TransferInterop.kt
+++ compat/transfer-api-compat/src/main/kotlin/xyz/bluspring/kilt/compat/transfer/TransferInterop.kt
@@ -1,5 +1,6 @@
 package xyz.bluspring.kilt.compat.transfer
 
+import io.github.fabricators_of_create.porting_lib.transfer.item.ItemItemStorages
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage
 import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage
@@ -18,6 +19,7 @@ import xyz.bluspring.kilt.compat.transfer.energy.FabricEnergyStorageCapability
 import xyz.bluspring.kilt.compat.transfer.energy.FabricEnergyStorageCapabilityProvider
 import xyz.bluspring.kilt.compat.transfer.energy.ForgeEnergyStorage
 import xyz.bluspring.kilt.compat.transfer.fluid.*
+import xyz.bluspring.kilt.compat.transfer.item.FabricItemItemStorageCapabilityProvider
 import xyz.bluspring.kilt.compat.transfer.item.FabricItemStorageCapability
 import xyz.bluspring.kilt.compat.transfer.item.FabricItemStorageCapabilityProvider
 import xyz.bluspring.kilt.compat.transfer.item.ForgeSlottedStorage
@@ -35,6 +37,23 @@ class TransferInterop : ModInitializer {
 
             if (itemHandlerCapability.isPresent) {
                 val handler = itemHandlerCapability.resolve().get()
+                if (handler !is FabricItemStorageCapability) {
+                    return@registerFallback ForgeSlottedStorage(handler)
+                }
+            }
+
+            null
+        }
+
+        ItemItemStorages.ITEM.registerFallback { stack, context ->
+            if (stack == null)
+                return@registerFallback null
+
+            val itemHandlerCapability = stack.getCapability(ForgeCapabilities.ITEM_HANDLER)
+
+            if (itemHandlerCapability.isPresent) {
+                val handler = itemHandlerCapability.resolve().get()
+
                 if (handler !is FabricItemStorageCapability) {
                     return@registerFallback ForgeSlottedStorage(handler)
                 }
@@ -111,26 +130,39 @@ class TransferInterop : ModInitializer {
     @SubscribeEvent
     fun onAttachBlockEntityCapabilities(event: AttachCapabilitiesEvent<BlockEntity>) {
         val blockEntity = event.`object`
-        event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_item_storage"),
-            FabricItemStorageCapabilityProvider(blockEntity)
-        )
-        event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_fluid_storage"),
-            FabricFluidStorageCapabilityProvider(blockEntity)
-        )
-        event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_energy_storage"),
-            FabricEnergyStorageCapabilityProvider(blockEntity)
-        )
+        if (ItemStorage.SIDED.getProvider(blockEntity.blockState.block) != null)
+            event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_item_storage"),
+                FabricItemStorageCapabilityProvider(blockEntity)
+            )
+
+        if (FluidStorage.SIDED.getProvider(blockEntity.blockState.block) != null)
+            event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_fluid_storage"),
+                FabricFluidStorageCapabilityProvider(blockEntity)
+            )
+
+        if (EnergyStorage.SIDED.getProvider(blockEntity.blockState.block) != null)
+            event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_energy_storage"),
+                FabricEnergyStorageCapabilityProvider(blockEntity)
+            )
     }
 
     @SubscribeEvent
     fun onAttachItemStackCapabilities(event: AttachCapabilitiesEvent<ItemStack>) {
         val stack = event.`object`
-        event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_fluid_item_storage"),
-            FabricFluidItemStorageCapabilityProvider(stack)
-        )
-        event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_energy_item_storage"),
-            FabricEnergyItemStorageCapabilityProvider(stack)
-        )
+        if (ItemItemStorages.ITEM.getProvider(stack.item) != null)
+            event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_item_item_storage"),
+                FabricItemItemStorageCapabilityProvider(stack)
+            )
+
+        if (FluidStorage.ITEM.getProvider(stack.item) != null)
+            event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_fluid_item_storage"),
+                FabricFluidItemStorageCapabilityProvider(stack)
+            )
+
+        if (EnergyStorage.ITEM.getProvider(stack.item) != null)
+            event.addCapability(ResourceLocation(Kilt.MOD_ID, "fabric_energy_item_storage"),
+                FabricEnergyItemStorageCapabilityProvider(stack)
+            )
     }
 
     companion object {
---
+++ compat/transfer-api-compat/src/main/kotlin/xyz/bluspring/kilt/compat/transfer/item/FabricItemItemStorageCapabilityProvider.kt
@@ -0,0 +1,31 @@
+package xyz.bluspring.kilt.compat.transfer.item
+
+import io.github.fabricators_of_create.porting_lib.transfer.item.ItemItemStorages
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant
+import net.fabricmc.fabric.api.transfer.v1.storage.SlottedStorage
+import net.minecraft.world.item.ItemStack
+import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.common.capabilities.ForgeCapabilities
+import net.minecraftforge.common.capabilities.ICapabilityProvider
+import net.minecraftforge.common.util.LazyOptional
+
+class FabricItemItemStorageCapabilityProvider(val stack: ItemStack) : ICapabilityProvider {
+    override fun <T : Any?> getCapability(cap: Capability<T>): LazyOptional<T> {
+        if (cap == ForgeCapabilities.ITEM_HANDLER) {
+            val fabricStorage = ItemItemStorages.ITEM.getProvider(stack.item) ?: return LazyOptional.empty()
+            val storage = fabricStorage.find(stack, null) ?: return LazyOptional.empty()
+
+            // Ignore our own storage
+            if (storage is ForgeSlottedStorage)
+                return LazyOptional.empty()
+
+            // Forge's transfer API is effectively slot-based, so fallback to a wrapper for Fabric's transfer to be slotted.
+            if (storage !is SlottedStorage<ItemVariant>)
+                return LazyOptional.of { FabricItemStorageCapability(FabricStorageWrapper(storage)) }.cast()
+
+            return LazyOptional.of { FabricItemStorageCapability(storage) }.cast()
+        }
+
+        return LazyOptional.empty()
+    }
+}
