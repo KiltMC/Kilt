b8cedca74c354c26fe2f703bea216b11aded96d7
Naz <bluspringindohq@gmail.com>
2025-05-21 19:29:01 +0800

    Fix Supplementaries and Copycats advancement incompatibility

--- src/main/java/xyz/bluspring/kilt/forgeinjects/server/ServerAdvancementManagerInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/server/ServerAdvancementManagerInject.java
@@ -1,6 +1,8 @@
 package xyz.bluspring.kilt.forgeinjects.server;
 
 import com.google.gson.JsonObject;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Cancellable;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.advancements.Advancement;
@@ -14,7 +16,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.bluspring.kilt.helpers.mixin.CreateInitializer;
 import xyz.bluspring.kilt.injections.advancements.AdvancementBuilderInjection;
@@ -38,16 +39,20 @@ public abstract class ServerAdvancementManagerInject implements ServerAdvancemen
         this.context = context;
     }
 
-    @Redirect(method = "method_20723", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/Advancement$Builder;fromJson(Lcom/google/gson/JsonObject;Lnet/minecraft/advancements/critereon/DeserializationContext;)Lnet/minecraft/advancements/Advancement$Builder;"))
-    private Advancement.Builder kilt$createBuilderWithContext(JsonObject json, DeserializationContext context, @Cancellable CallbackInfo ci, @Local(argsOnly = true) ResourceLocation location) {
-        var builder = AdvancementBuilderInjection.fromJson(json, context, this.context);
-        if (builder == null) {
-            LOGGER.debug("Skipping loading advancement {} as its conditions were not met.", location);
-            ci.cancel();
-            return null;
-        }
+    @WrapOperation(method = "method_20723", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/Advancement$Builder;fromJson(Lcom/google/gson/JsonObject;Lnet/minecraft/advancements/critereon/DeserializationContext;)Lnet/minecraft/advancements/Advancement$Builder;"))
+    private Advancement.Builder kilt$createBuilderWithContext(JsonObject json, DeserializationContext context, Operation<Advancement.Builder> original, @Cancellable CallbackInfo ci, @Local(argsOnly = true) ResourceLocation location) {
+        try {
+            var builder = AdvancementBuilderInjection.fromJson(json, context, this.context);
+            if (builder == null) {
+                LOGGER.debug("Skipping loading advancement {} as its conditions were not met.", location);
+                ci.cancel();
+                return null;
+            }
 
-        return builder;
+            return builder;
+        } catch (Exception ignored) {
+            return original.call(json, context);
+        }
     }
 
 
