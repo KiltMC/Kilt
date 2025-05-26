8d9a5ddc9480aa03571ec80456f7933b565e1ed5
Naz <bluspringindohq@gmail.com>
2025-05-27 02:54:12 +0800

    Some additional block inject completions

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/block/BlockInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/block/BlockInject.java
@@ -1,13 +1,14 @@
 // TRACKED HASH: cfca572d6c2e46ca49cb8ffbdda7f12c0d1c4d30
 package xyz.bluspring.kilt.forgeinjects.world.level.block;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.IdMapper;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
@@ -27,10 +28,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import xyz.bluspring.kilt.helpers.mixin.CreateStatic;
 import xyz.bluspring.kilt.injections.client.renderer.RenderPropertiesInjection;
+import xyz.bluspring.kilt.injections.world.level.LevelInjection;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -56,18 +56,37 @@ public abstract class BlockInject implements IForgeBlock, RenderPropertiesInject
         }
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;skipRendering(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;)Z", shift = At.Shift.BEFORE), method = "shouldRenderFace", locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private static void kilt$handleRenderFace(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction, BlockPos blockPos2, CallbackInfoReturnable<Boolean> cir, BlockState blockState2) {
-        if (blockState.skipRendering(blockState2, direction))
-            cir.setReturnValue(false);
-        else if (blockState.supportsExternalFaceHiding() && blockState2.hidesNeighborFace(blockGetter, blockPos, blockState, direction))
-            cir.setReturnValue(false);
+    @ModifyExpressionValue(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;skipRendering(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;)Z"), method = "shouldRenderFace")
+    private static boolean kilt$handleRenderFace(boolean original, @Local(argsOnly = true) BlockState blockState, @Local(ordinal = 1) BlockState blockState2, @Local(argsOnly = true) Direction direction, @Local(argsOnly = true) BlockGetter blockGetter, @Local(argsOnly = true, ordinal = 0) BlockPos blockPos) {
+        return original || (blockState.supportsExternalFaceHiding() && blockState2.hidesNeighborFace(blockGetter, blockPos, blockState, direction.getOpposite()));
     }
 
-    @WrapOperation(at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/Level;isClientSide:Z"), method = "popResource(Lnet/minecraft/world/level/Level;Ljava/util/function/Supplier;Lnet/minecraft/world/item/ItemStack;)V")
-    private static boolean kilt$checkRestoringBlockSnapshots(Level instance, Operation<Boolean> original) {
-        // TODO: actually implement this
-        return original.call(instance); //&& !((IForgeLevel) instance).restoringBlockSnapshots;
+    @Unique
+    private static final AtomicBoolean kilt$dropXp = new AtomicBoolean(false);
+
+    @ModifyArg(method = "dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;spawnAfterBreak(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/item/ItemStack;Z)V"))
+    private static boolean kilt$dropResourcesXp(boolean original) {
+        if (!original)
+            return true;
+
+        return kilt$dropXp.getAndSet(false);
+    }
+
+    @CreateStatic
+    private static void dropResources(BlockState state, Level level, BlockPos pos, @Nullable BlockEntity blockEntity, @Nullable Entity entity, ItemStack tool, boolean dropXp) {
+        kilt$dropXp.set(dropXp);
+        Block.dropResources(state, level, pos, blockEntity, entity, tool);
+        kilt$dropXp.set(false);
+    }
+
+    @ModifyExpressionValue(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/GameRules;getBoolean(Lnet/minecraft/world/level/GameRules$Key;)Z", ordinal = 0), method = "popResource(Lnet/minecraft/world/level/Level;Ljava/util/function/Supplier;Lnet/minecraft/world/item/ItemStack;)V")
+    private static boolean kilt$checkRestoringBlockSnapshots(boolean original, @Local(argsOnly = true) Level level) {
+        return original && !((LevelInjection) level).kilt$getRestoringBlockSnapshots();
+    }
+
+    @ModifyExpressionValue(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/GameRules;getBoolean(Lnet/minecraft/world/level/GameRules$Key;)Z"), method = "popExperience")
+    private boolean kilt$checkRestoringBlockSnapshots(boolean original, @Local(argsOnly = true) ServerLevel level) {
+        return original && !((LevelInjection) level).kilt$getRestoringBlockSnapshots();
     }
 
     @Override
@@ -117,23 +136,5 @@ public abstract class BlockInject implements IForgeBlock, RenderPropertiesInject
             return isBeach && hasWater;
         }
         return false;
-    }
-
-    @Unique
-    private static final AtomicBoolean kilt$dropXp = new AtomicBoolean(false);
-
-    @CreateStatic
-    private static void dropResources(BlockState state, Level level, BlockPos pos, @Nullable BlockEntity blockEntity, @Nullable Entity entity, ItemStack tool, boolean dropXp) {
-        kilt$dropXp.set(dropXp);
-        Block.dropResources(state, level, pos, blockEntity, entity, tool);
-        kilt$dropXp.set(false);
-    }
-
-    @ModifyArg(method = "dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;spawnAfterBreak(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/item/ItemStack;Z)V"))
-    private static boolean kilt$dropResourcesXp(boolean original) {
-        if (!original)
-            return true;
-
-        return kilt$dropXp.getAndSet(false);
     }
 }
