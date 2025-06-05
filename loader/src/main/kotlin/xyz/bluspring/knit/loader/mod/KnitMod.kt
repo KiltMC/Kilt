9a48500dacebf79a348dac5824af6c8ef040048d
Naz <bluspringindohq@gmail.com>
2025-06-05 23:46:26 +0800

    bruh

--- loader/src/main/kotlin/xyz/bluspring/knit/loader/mod/KnitMod.kt
+++
@@ -1,8 +0,0 @@
-package xyz.bluspring.knit.loader.mod
-
-/**
- * Stores information about the mod that Knit can store and other loaders to use.
- */
-abstract class KnitMod(val definition: ModDefinition) {
-
-}
