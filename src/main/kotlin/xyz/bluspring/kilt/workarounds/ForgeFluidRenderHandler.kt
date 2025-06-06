69c4cc86b1477e85f513088414f74a3e1b818e68
Naz <bluspringindohq@gmail.com>
2025-06-07 02:43:27 +0800

    Fix inverted fluid colours. again.

--- src/main/kotlin/xyz/bluspring/kilt/workarounds/ForgeFluidRenderHandler.kt
+++ src/main/kotlin/xyz/bluspring/kilt/workarounds/ForgeFluidRenderHandler.kt
@@ -3,7 +3,6 @@ package xyz.bluspring.kilt.workarounds
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
 import net.minecraft.core.BlockPos
-import net.minecraft.util.FastColor
 import net.minecraft.world.level.BlockAndTintGetter
 import net.minecraft.world.level.material.FluidState
 import net.minecraftforge.client.ForgeHooksClient
@@ -19,11 +18,6 @@ class ForgeFluidRenderHandler : FluidRenderHandler {
     }
 
     override fun getFluidColor(view: BlockAndTintGetter?, pos: BlockPos?, state: FluidState?): Int {
-        val bgr = IClientFluidTypeExtensions.of(state).getTintColor(state, view, pos)
-        val r = FastColor.ABGR32.red(bgr)
-        val g = FastColor.ABGR32.green(bgr)
-        val b = FastColor.ABGR32.blue(bgr)
-        val a = FastColor.ABGR32.alpha(bgr)
-        return FastColor.ARGB32.color(a, r, g, b)
+        return IClientFluidTypeExtensions.of(state).getTintColor(state, view, pos)
     }
 }
