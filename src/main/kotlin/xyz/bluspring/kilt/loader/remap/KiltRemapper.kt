0d9bb59c5800f8cbef36f0c8645dd343540a2d45
Naz <bluspringindohq@gmail.com>
2025-06-18 15:57:17 +0800

    has that seriously been the remapper issue

--- src/main/kotlin/xyz/bluspring/kilt/loader/remap/KiltRemapper.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/remap/KiltRemapper.kt
@@ -51,7 +51,7 @@ object KiltRemapper {
     // Keeps track of the remapper changes, so every time I update the remapper,
     // it remaps all the mods following the remapper changes.
     // this can update by like 12 versions in 1 update, so don't worry too much about it.
-    const val REMAPPER_VERSION = 176
+    const val REMAPPER_VERSION = 178
     const val MC_MAPPED_JAR_VERSION = 3
 
     // Kilt JVM flags
@@ -136,10 +136,6 @@ object KiltRemapper {
             launch(Dispatchers.IO) {
                 srgIntermediaryMapping.classes.asFlow().concurrent().collect {
                     it.methods.asFlow().concurrent().collect { f ->
-                        // otherwise FunctionalInterface methods don't get remapped properly???
-                        if (!f.mapped.startsWith("method_") && !FabricLoader.getInstance().isDevelopmentEnvironment)
-                            return@collect
-
                         val map = srgMappedMethods.getOrPut(f.original) {
                             Object2ReferenceMaps.synchronize(Object2ReferenceOpenHashMap())
                         }
@@ -174,6 +170,10 @@ object KiltRemapper {
 
         if (forceRemap)
             logger.warn("Forced remaps enabled! All Forge mods will be remapped.")
+
+        // Forcefully initialize the mapping resolver.
+        // Otherwise, it just crashes from trying to initialize concurrently.
+        mappingResolver.unmapClassName("intermediary", "net.minecraft.class_1937")
 
         // Automatically delete outdated remapped versions
         run {
