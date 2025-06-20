e905464a64b8bb3098620079caa81f94b1a976e6
Naz <bluspringindohq@gmail.com>
2025-06-21 02:10:26 +0800

    fixed crash with Supplementaries and Alex's Mobs

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/screens/inventory/tooltip/ClientTooltipComponentInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/screens/inventory/tooltip/ClientTooltipComponentInject.java
@@ -1,5 +1,6 @@
 package xyz.bluspring.kilt.forgeinjects.client.gui.screens.inventory.tooltip;
 
+import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraftforge.client.gui.ClientTooltipComponentManager;
@@ -13,6 +14,14 @@ public interface ClientTooltipComponentInject {
     @Inject(at = @At(value = "INVOKE", target = "Ljava/lang/IllegalArgumentException;<init>(Ljava/lang/String;)V", shift = At.Shift.BEFORE), method = "create(Lnet/minecraft/world/inventory/tooltip/TooltipComponent;)Lnet/minecraft/client/gui/screens/inventory/tooltip/ClientTooltipComponent;", cancellable = true)
     private static void kilt$getForgeClientTooltip(TooltipComponent tooltipComponent, CallbackInfoReturnable<ClientTooltipComponent> cir) {
         var result = ClientTooltipComponentManager.createClientTooltipComponent(tooltipComponent);
+        if (result != null) {
+            cir.setReturnValue(result);
+            return;
+        }
+
+        // Kilt: this isn't handled in Fabric API, so we have to do this ourselves to avoid a crash with Supplementaries.
+        //       yes, Supplementaries.
+        result = TooltipComponentCallback.EVENT.invoker().getComponent(tooltipComponent);
         if (result != null)
             cir.setReturnValue(result);
     }
