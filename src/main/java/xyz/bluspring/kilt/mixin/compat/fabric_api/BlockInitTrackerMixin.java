72df8b7833326a7e8a1dd80bc85cb2dea7ec5def
AlphaMode <26313415+AlphaMode@users.noreply.github.com>
2025-06-22 01:22:53 -0500

    Alpha based moment who ignores prod v2 electric boogaloo (#325)
    * EvilCraft works now
    
    * Resolve PR issues
    Stop depending on cyclopscore
    Change VertexConsumerInject#kilt$applyLightmap to use ordinal for targeting
    
    * Fix remap issue in prod
    
    * More prod fixes

--- src/main/java/xyz/bluspring/kilt/mixin/compat/fabric_api/BlockInitTrackerMixin.java
+++ src/main/java/xyz/bluspring/kilt/mixin/compat/fabric_api/BlockInitTrackerMixin.java
@@ -8,7 +8,7 @@ import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = BlockInitTracker.class, remap = false)
+@Mixin(BlockInitTracker.class)
 public class BlockInitTrackerMixin {
     // Fabric api just randomly calls this for whatever reason? It causes forge mods that used Deferred Objects to error so uhh just ignore it lol
     @WrapOperation(method = "onEntryAdded(ILnet/minecraft/resources/ResourceLocation;Lnet/minecraft/world/level/block/Block;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;getLootTable()Lnet/minecraft/resources/ResourceLocation;"))
