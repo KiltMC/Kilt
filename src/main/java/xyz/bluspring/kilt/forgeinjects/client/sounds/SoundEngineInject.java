6ac864dfa0fe8db2afa8871e46971f454563637b
Naz <bluspringindohq@gmail.com>
2025-07-04 10:38:10 +0800

    Fix TACZ sound engine bug (closes #328)

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/sounds/SoundEngineInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/sounds/SoundEngineInject.java
@@ -3,7 +3,9 @@ package xyz.bluspring.kilt.forgeinjects.client.sounds;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import com.mojang.blaze3d.audio.Channel;
 import com.mojang.blaze3d.audio.Library;
 import net.minecraft.client.resources.sounds.Sound;
 import net.minecraft.client.resources.sounds.SoundInstance;
@@ -15,8 +17,13 @@ import net.minecraftforge.fml.ModLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.bluspring.kilt.helpers.SoundConsumerStorage;
 import xyz.bluspring.kilt.injections.client.sounds.ChannelAccessHandleInjection;
+
+import java.util.UUID;
+import java.util.function.Consumer;
 
 @Mixin(SoundEngine.class)
 public abstract class SoundEngineInject {
@@ -42,6 +49,18 @@ public abstract class SoundEngineInject {
 
         injection.kilt$setSoundInstance(soundInstance);
         injection.kilt$setSoundEngine((SoundEngine) (Object) this);
+    }
+
+    @ModifyArg(method = "method_19757", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sounds/ChannelAccess$ChannelHandle;execute(Ljava/util/function/Consumer;)V"))
+    private static Consumer<Channel> kilt$storeSourceConsumer(Consumer<Channel> consumer) {
+        SoundConsumerStorage.soundConsumerChannels.add(consumer);
+        return consumer;
+    }
+
+    @ModifyArg(method = "method_19758", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sounds/ChannelAccess$ChannelHandle;execute(Ljava/util/function/Consumer;)V"))
+    private static Consumer<Channel> kilt$storeStreamConsumer(Consumer<Channel> consumer) {
+        SoundConsumerStorage.soundConsumerChannels.add(consumer);
+        return consumer;
     }
 
     // Kilt: PlaySoundSourceEvent and PlayStreamingSourceEvent is handled in ChannelAccessHandleMixin
--- src/main/java/xyz/bluspring/kilt/injections/client/sounds/ChannelAccessHandleInjection.java
+++ src/main/java/xyz/bluspring/kilt/injections/client/sounds/ChannelAccessHandleInjection.java
@@ -4,6 +4,8 @@ import com.mojang.blaze3d.audio.Library;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.SoundEngine;
 
+import java.util.UUID;
+
 public interface ChannelAccessHandleInjection {
     void kilt$setPool(Library.Pool pool);
     void kilt$setSoundInstance(SoundInstance instance);
