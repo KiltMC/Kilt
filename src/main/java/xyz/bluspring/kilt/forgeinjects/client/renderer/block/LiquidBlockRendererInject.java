3b25b44181d2bd7aa79b0746e62b30648144531a
alpha <alphamodeyt@gmail.com>
2025-07-05 21:01:34 -0500

    Fix reign of nether rts

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/LiquidBlockRendererInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/renderer/block/LiquidBlockRendererInject.java
@@ -1,9 +1,14 @@
 // TRACKED HASH: 76d201a1a6836dbb6ce5a521f6f40e36bff0dc05
 package xyz.bluspring.kilt.forgeinjects.client.renderer.block;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.block.LiquidBlockRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(LiquidBlockRenderer.class)
 public class LiquidBlockRendererInject {
+    // Kilt: forge added method
+    private void vertex(VertexConsumer pConsumer, double pX, double pY, double pZ, float pRed, float pGreen, float pBlue, float alpha, float pU, float pV, int pPackedLight) {
+        pConsumer.vertex(pX, pY, pZ).color(pRed, pGreen, pBlue, alpha).uv(pU, pV).uv2(pPackedLight).normal(0.0F, 1.0F, 0.0F).endVertex();
+    }
 }
