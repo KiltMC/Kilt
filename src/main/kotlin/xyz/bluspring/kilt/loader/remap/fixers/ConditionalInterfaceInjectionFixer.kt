77bb4ffcbc43394fc81b0bed7c3496bb12c5f7ba
Naz <bluspringindohq@gmail.com>
2025-07-11 04:48:42 +0800

    Revert "Add condition interface injection for DamageableItem"
    This reverts commit 5e4650a62159de2e9ab091929e408f47070a2a0c.

--- src/main/kotlin/xyz/bluspring/kilt/loader/remap/fixers/ConditionalInterfaceInjectionFixer.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/remap/fixers/ConditionalInterfaceInjectionFixer.kt
@@ -1,16 +1,15 @@
 package xyz.bluspring.kilt.loader.remap.fixers
 
+import org.objectweb.asm.Opcodes
 import org.objectweb.asm.signature.SignatureReader
+import org.objectweb.asm.signature.SignatureVisitor
 import org.objectweb.asm.signature.SignatureWriter
 import org.objectweb.asm.tree.ClassNode
 import xyz.bluspring.kilt.loader.remap.KiltRemapper
 
 object ConditionalInterfaceInjectionFixer {
-    private const val PORTING_LIB_PACKAGE = "io/github/fabricators_of_create/porting_lib"
-
     private val forgeMethodLookup = mapOf(
-        ("getLightEmission" to KiltRemapper.remapDescriptor("(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)I")) to "$PORTING_LIB_PACKAGE/block/LightEmissiveBlock",
-        ("getDamage" to KiltRemapper.remapDescriptor("(Lnet/minecraft/world/item/ItemStack;)I")) to "$PORTING_LIB_PACKAGE/item/DamageableItem"
+        ("getLightEmission" to KiltRemapper.remapDescriptor("(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)I")) to "io/github/fabricators_of_create/porting_lib/block/LightEmissiveBlock"
     )
 
     fun fixClass(classNode: ClassNode) {
