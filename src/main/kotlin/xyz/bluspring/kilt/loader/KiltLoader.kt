4822ee91c347e70490e596c3dea5243c3c529130
Naz <bluspringindohq@gmail.com>
2025-07-15 16:13:07 +0800

    Avoid shadowing

--- src/main/kotlin/xyz/bluspring/kilt/loader/KiltLoader.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/KiltLoader.kt
@@ -535,27 +535,27 @@ class KiltLoader : KnitModLoader<ForgeMod>(Kilt.MOD_ID, "Forge") {
         // Automatically subscribe events
         scanData.annotations.asFlow()
             .filter { it.annotationType == AUTO_SUBSCRIBE_ANNOTATION }
-            .collect {
+            .collect { annotation ->
                 // it.annotationData["modid"] as String
                 // it.annotationData["bus"] as Mod.EventBusSubscriber.Bus
 
                 try {
-                    val modId = it.annotationData["modid"] as String?
+                    val modId = annotation.annotationData["modid"] as String?
                         // Use the mod ID of the mod in the class instead
-                        ?: scanData.annotations.firstOrNull { a -> checkTypeOrParentsAreType(a.clazz, it.clazz) && a.annotationType == MOD_ANNOTATION }?.annotationData?.get("value") as? String?
+                        ?: scanData.annotations.firstOrNull { a -> checkTypeOrParentsAreType(a.clazz, annotation.clazz) && a.annotationType == MOD_ANNOTATION }?.annotationData?.get("value") as? String?
                         ?: mod.modId
 
                     if (modId != mod.modId)
                         return@collect
 
                     val busType = Mod.EventBusSubscriber.Bus.valueOf(
-                        if (it.annotationData.contains("bus"))
-                            (it.annotationData["bus"] as ModAnnotation.EnumHolder).value!!
+                        if (annotation.annotationData.contains("bus"))
+                            (annotation.annotationData["bus"] as ModAnnotation.EnumHolder).value!!
                         else "FORGE"
                     )
 
-                    val dists = if (it.annotationData.contains("value"))
-                        (it.annotationData["value"] as List<ModAnnotation.EnumHolder>).map { Dist.valueOf(it.value!!) }
+                    val dists = if (annotation.annotationData.contains("value"))
+                        (annotation.annotationData["value"] as List<ModAnnotation.EnumHolder>).map { Dist.valueOf(it.value!!) }
                     else
                         listOf()
 
@@ -565,7 +565,7 @@ class KiltLoader : KnitModLoader<ForgeMod>(Kilt.MOD_ID, "Forge") {
 
                     ModLoadingContext.kiltActiveModId = modId
 
-                    val clazz = Class.forName(it.clazz.className, true, this::class.java.classLoader)
+                    val clazz = Class.forName(annotation.clazz.className, true, this::class.java.classLoader)
                     val obj = try { clazz.kotlin.objectInstance } catch (_: Throwable) { null }
 
                     if (obj != null)
@@ -575,11 +575,12 @@ class KiltLoader : KnitModLoader<ForgeMod>(Kilt.MOD_ID, "Forge") {
 
                     ModLoadingContext.kiltActiveModId = null
 
-                    Kilt.logger.debug("Automatically registered event ${it.clazz.className} from mod ID $modId under bus ${busType.name}")
+                    Kilt.logger.debug("Automatically registered event ${annotation.clazz.className} from mod ID $modId under bus ${busType.name}")
                 } catch (e: Throwable) {
-                    Kilt.logger.error("Failed to register event ${it.clazz.className} from mod ${mod.modId}!")
-                    e.printStackTrace()
-                    exception.addSuppressed(e)
+                    Kilt.logger.error("Failed to register event ${annotation.clazz.className} from mod ${mod.modId}!")
+                    val ex = RuntimeException("Failed to register event ${annotation.clazz.className} from mod ${mod.modId}!", e)
+                    ex.printStackTrace()
+                    exception.addSuppressed(ex)
                 }
             }
 
