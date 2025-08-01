318adb3678debeab2d53eaf4bc90e84a0bfbe0fb
Naz <bluspringindohq@gmail.com>
2025-08-02 00:52:14 +0800

    Workaround to try to fix the fluid stack overflow

--- src/main/kotlin/xyz/bluspring/kilt/workarounds/FabricFluidTypeExtensions.kt
+++ src/main/kotlin/xyz/bluspring/kilt/workarounds/FabricFluidTypeExtensions.kt
@@ -10,36 +10,48 @@ import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions
 
 class FabricFluidTypeExtensions(private val fluid: Fluid) : IClientFluidTypeExtensions {
     private val renderHandler = FluidRenderHandlerRegistry.INSTANCE.get(fluid)
+    private val isInvalid: Boolean
+        get() {
+            return renderHandler is ForgeFluidRenderHandler
+        }
 
-    override fun getStillTexture(): ResourceLocation {
+    override fun getStillTexture(): ResourceLocation? {
+        if (isInvalid) return null
         return renderHandler.getFluidSprites(null, null, fluid.defaultFluidState())[0].contents().name()
     }
 
-    override fun getFlowingTexture(): ResourceLocation {
+    override fun getFlowingTexture(): ResourceLocation? {
+        if (isInvalid) return null
         return renderHandler.getFluidSprites(null, null, fluid.defaultFluidState())[1].contents().name()
     }
 
     override fun getOverlayTexture(): ResourceLocation? {
+        if (isInvalid) return null
         return renderHandler.getFluidSprites(null, null, fluid.defaultFluidState()).getOrNull(2)?.contents()?.name()
     }
 
     override fun getStillTexture(state: FluidState, getter: BlockAndTintGetter?, pos: BlockPos?): ResourceLocation? {
+        if (isInvalid) return null
         return renderHandler.getFluidSprites(getter, pos, state)[0].contents().name()
     }
 
     override fun getFlowingTexture(state: FluidState?, getter: BlockAndTintGetter?, pos: BlockPos?): ResourceLocation? {
+        if (isInvalid) return null
         return renderHandler.getFluidSprites(getter, pos, state)[1].contents().name()
     }
 
     override fun getOverlayTexture(state: FluidState?, getter: BlockAndTintGetter?, pos: BlockPos?): ResourceLocation? {
+        if (isInvalid) return null
         return renderHandler.getFluidSprites(getter, pos, state).getOrNull(2)?.contents()?.name()
     }
 
     override fun getTintColor(): Int {
+        if (isInvalid) return -1
         return renderHandler.getFluidColor(null, null, fluid.defaultFluidState())
     }
 
     override fun getTintColor(state: FluidState?, getter: BlockAndTintGetter?, pos: BlockPos?): Int {
+        if (isInvalid) return -1
         return renderHandler.getFluidColor(getter, pos, state)
     }
 }
