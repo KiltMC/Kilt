939c63f0fa2955b5a5c58c23402a9d7d08d4f660
Naz <bluspringindohq@gmail.com>
2025-08-02 20:37:39 +0800

    Fix keybinds not allowing only modifier keys

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/screens/controls/KeyBindsListInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/gui/screens/controls/KeyBindsListInject.java
@@ -1,81 +1,52 @@
 // TRACKED HASH: dc2f14e758389a304a6ed1f74286d07572328592
 package xyz.bluspring.kilt.forgeinjects.client.gui.screens.controls;
 
-import net.minecraft.ChatFormatting;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.controls.KeyBindsList;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraftforge.client.extensions.IForgeKeyMapping;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.bluspring.kilt.mixin.KeyBindsListAccessor;
 
 @Mixin(KeyBindsList.class)
-public class KeyBindsListInject {
+public abstract class KeyBindsListInject {
     @Mixin(KeyBindsList.KeyEntry.class)
-    public static class KeyEntryInject {
+    public abstract static class KeyEntryInject {
         @Shadow @Final private Button changeButton;
-
         @Shadow @Final private KeyMapping key;
-
         @Shadow private boolean hasCollision;
-
         @Shadow @Final private Button resetButton;
-
         @Shadow @Final KeyBindsList field_2742;
+
+        @Inject(method = "method_19871", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/controls/KeyBindsList;resetMappingAndUpdateButtons()V"))
+        private void kilt$resetKeyModifierAndCode(KeyMapping keyMapping, Button button, CallbackInfo ci) {
+            keyMapping.setKeyModifierAndCode(null, InputConstants.UNKNOWN);
+        }
 
         @Inject(method = "method_19870", at = @At("HEAD"))
         private void kilt$setKeyToDefault(KeyMapping keyMapping, Button button, CallbackInfo ci) {
             ((IForgeKeyMapping) this.key).setToDefault();
         }
 
-        /**
-         * @author BluSpring
-         * @reason I have no idea how to do this patch otherwise
-         */
-        // fun fact: mixin was yelling at me saying Overwrite can't reduce the visibility of refreshEntry().
-        // refreshEntry is normally protected.
-        @Overwrite public void refreshEntry() {
-            this.changeButton.setMessage(this.key.getTranslatedKeyMessage());
-            this.resetButton.active = !this.key.isDefault();
-            this.hasCollision = false;
-            MutableComponent mutableComponent = Component.empty();
-            if (!this.key.isUnbound()) {
-                KeyMapping[] var2 = Minecraft.getInstance().options.keyMappings;
-
-                for (KeyMapping keyMapping : var2) {
-                    // this is the patch we're doing.
-                    // TODO: can we avoid an overwrite?
-                    if ((keyMapping != this.key && this.key.same(keyMapping)) || ((IForgeKeyMapping) keyMapping).hasKeyModifierConflict(this.key)) {
-                        if (this.hasCollision) {
-                            mutableComponent.append(", ");
-                        }
-
-                        this.hasCollision = true;
-                        mutableComponent.append(Component.translatable(keyMapping.getName()));
-                    }
-                }
-            }
-
-            if (this.hasCollision) {
-                this.changeButton.setMessage(Component.literal("[ ").append(this.changeButton.getMessage().copy().withStyle(ChatFormatting.WHITE)).append(" ]").withStyle(ChatFormatting.RED));
-                this.changeButton.setTooltip(Tooltip.create(Component.translatable("controls.keybinds.duplicateKeybinds", new Object[]{mutableComponent})));
-            } else {
-                this.changeButton.setTooltip(null);
-            }
-
-            if (((KeyBindsListAccessor) field_2742).getKeyBindsScreen().selectedKey == this.key) {
-                this.changeButton.setMessage(Component.literal("> ").append(this.changeButton.getMessage().copy().withStyle(new ChatFormatting[]{ChatFormatting.WHITE, ChatFormatting.UNDERLINE})).append(" <").withStyle(ChatFormatting.YELLOW));
-            }
+        // Kilt: figured it out!
+        @Definition(id = "keyMapping", local = @Local(type = KeyMapping.class))
+        @Definition(id = "key", field = "Lnet/minecraft/client/gui/screens/controls/KeyBindsList$KeyEntry;key:Lnet/minecraft/client/KeyMapping;")
+        @Expression("keyMapping != this.key")
+        @ModifyExpressionValue(method = "refreshEntry", at = {
+            @At("MIXINEXTRAS:EXPRESSION"),
+            @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;same(Lnet/minecraft/client/KeyMapping;)Z")
+        })
+        private boolean kilt$checkHasKeyModifierConflict(boolean original, @Local KeyMapping keyMapping) {
+            return original || ((IForgeKeyMapping) keyMapping).hasKeyModifierConflict(this.key);
         }
     }
 }
