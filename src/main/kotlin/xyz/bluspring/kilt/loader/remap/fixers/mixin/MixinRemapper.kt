e360c40af1512c0f51b337a1f1d63d5f20c887a2
Naz <bluspringindohq@gmail.com>
2025-08-29 17:25:43 +0800

    Fix weird refmap crash in mixin remapper

--- src/main/kotlin/xyz/bluspring/kilt/loader/remap/fixers/mixin/MixinRemapper.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/remap/fixers/mixin/MixinRemapper.kt
@@ -227,7 +227,8 @@ object MixinRemapper {
     fun remapUnmappedRefmaps(refmaps: Collection<JsonObject>, remapper: KiltEnhancedRemapper) {
         for (refmap in refmaps) {
             val alreadyRefmapped = if (refmap.has("kilt:alreadyRefmapped"))
-                refmap.getAsJsonObject("kilt:alreadyRefmapped").asMap().filterValues { it != null }.map { it.key to it.value.asJsonArray.map { e -> e.asString } }.associate { it.first to it.second }
+                refmap.getAsJsonObject("kilt:alreadyRefmapped")?.asMap()?.filterValues { it != null }?.map { it.key to it.value.asJsonArray.map { e -> e.asString } }?.associate { it.first to it.second }
+                    ?: mapOf() // Why and how this is even possible is completely beyond me.
             else
                 mapOf()
 
