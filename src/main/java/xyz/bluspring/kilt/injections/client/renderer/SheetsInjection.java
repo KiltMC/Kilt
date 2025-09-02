ea19b80707289c9c3e1aa007494f507ea7ffddd9
Naz <bluspringindohq@gmail.com>
2025-09-02 11:17:13 +0800

    Try to fix crash with Supplementaries + some early sheet stuff

--- src/main/java/xyz/bluspring/kilt/injections/client/renderer/SheetsInjection.java
+++ src/main/java/xyz/bluspring/kilt/injections/client/renderer/SheetsInjection.java
@@ -1,12 +1,35 @@
 package xyz.bluspring.kilt.injections.client.renderer;
 
 import net.minecraft.client.renderer.Sheets;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import xyz.bluspring.kilt.mixin.SheetsAccessor;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public interface SheetsInjection {
+    AtomicBoolean kilt$hasSheetsLoaded = new AtomicBoolean(false);
+
     static void addWoodType(WoodType woodType) {
         Sheets.SIGN_MATERIALS.put(woodType, SheetsAccessor.callCreateSignMaterial(woodType));
         Sheets.HANGING_SIGN_MATERIALS.put(woodType, SheetsAccessor.callCreateHangingSignMaterial(woodType));
+    }
+
+    // otherwise the patterns don't exist if Fabric mods load them first, and Supplementaries panics.
+    static void kilt$resyncBannerSheetLayers() {
+        if (!kilt$hasSheetsLoaded.get()) // We don't need to resync if it hasn't even loaded yet.
+            return;
+
+        SheetsAccessor.setBannerMaterials(BuiltInRegistries.BANNER_PATTERN.registryKeySet().stream().collect(Collectors.toMap(Function.identity(), SheetsAccessor::callCreateBannerMaterial)));
+        SheetsAccessor.setShieldMaterials(BuiltInRegistries.BANNER_PATTERN.registryKeySet().stream().collect(Collectors.toMap(Function.identity(), SheetsAccessor::callCreateShieldMaterial)));
+    }
+
+    static void kilt$resyncDecoratedPotSheetLayers() {
+        if (!kilt$hasSheetsLoaded.get())
+            return;
+
+        SheetsAccessor.setDecoratedPotMaterials(BuiltInRegistries.DECORATED_POT_PATTERNS.registryKeySet().stream().collect(Collectors.toMap(Function.identity(), SheetsAccessor::callCreateDecoratedPotMaterial)));
     }
 }
--- src/main/java/xyz/bluspring/kilt/mixin/SheetsAccessor.java
+++ src/main/java/xyz/bluspring/kilt/mixin/SheetsAccessor.java
@@ -2,9 +2,14 @@ package xyz.bluspring.kilt.mixin;
 
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.resources.model.Material;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.entity.BannerPattern;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.Map;
 
 @Mixin(Sheets.class)
 public interface SheetsAccessor {
@@ -15,6 +20,36 @@ public interface SheetsAccessor {
 
     @Invoker
     static Material callCreateHangingSignMaterial(WoodType woodType) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Invoker
+    static Material callCreateBannerMaterial(ResourceKey<BannerPattern> key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Invoker
+    static Material callCreateShieldMaterial(ResourceKey<BannerPattern> key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Invoker
+    static Material callCreateDecoratedPotMaterial(ResourceKey<String> key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Accessor("DECORATED_POT_MATERIALS")
+    static void setDecoratedPotMaterials(Map<ResourceKey<String>, Material> value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Accessor("SHIELD_MATERIALS")
+    static void setShieldMaterials(Map<ResourceKey<BannerPattern>, Material> value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Accessor("BANNER_MATERIALS")
+    static void setBannerMaterials(Map<ResourceKey<BannerPattern>, Material> value) {
         throw new UnsupportedOperationException();
     }
 }
