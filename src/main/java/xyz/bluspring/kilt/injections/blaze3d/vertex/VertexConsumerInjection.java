ac5628898fd3b3aa38e7d591dfa3657fe826b08e
Naz <bluspringindohq@gmail.com>
2025-09-25 12:28:06 +0800

    Actually make it handle multithreaded situations, probably.

--- src/main/java/xyz/bluspring/kilt/injections/blaze3d/vertex/VertexConsumerInjection.java
+++ src/main/java/xyz/bluspring/kilt/injections/blaze3d/vertex/VertexConsumerInjection.java
@@ -2,12 +2,10 @@ package xyz.bluspring.kilt.injections.blaze3d.vertex;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.block.model.BakedQuad;
-import xyz.bluspring.kilt.util.FloatReference;
-
-import java.util.concurrent.atomic.AtomicReference;
+import xyz.bluspring.kilt.util.ConcurrentFloatReference;
 
 public interface VertexConsumerInjection {
-    FloatReference alpha = new FloatReference();
+    ConcurrentFloatReference alpha = new ConcurrentFloatReference();
 
     default void putBulkData(PoseStack.Pose pose, BakedQuad bakedQuad, float[] fs, float f, float g, float h, float alpha, int[] is, int i, boolean bl) {
         throw new IllegalStateException();
