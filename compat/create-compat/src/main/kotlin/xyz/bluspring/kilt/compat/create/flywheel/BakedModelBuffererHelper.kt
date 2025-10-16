a71150058861d0d20f036940ca8ec8c2697d92d6
Naz <bluspringindohq@gmail.com>
2025-10-16 15:19:54 +0800

    Fix Create rendering

---
+++ compat/create-compat/src/main/kotlin/xyz/bluspring/kilt/compat/create/flywheel/BakedModelBuffererHelper.kt
@@ -0,0 +1,13 @@
+package xyz.bluspring.kilt.compat.create.flywheel
+
+import net.minecraft.core.BlockPos
+import net.minecraftforge.client.model.data.ModelData
+import java.util.function.Function
+
+object BakedModelBuffererHelper {
+    @JvmStatic
+    val modelData: ThreadLocal<ModelData> = ThreadLocal.withInitial { ModelData.EMPTY }
+
+    @JvmStatic
+    val modelDataLookup: ThreadLocal<Function<BlockPos, ModelData>> = ThreadLocal.withInitial { Function { ModelData.EMPTY } }
+}
