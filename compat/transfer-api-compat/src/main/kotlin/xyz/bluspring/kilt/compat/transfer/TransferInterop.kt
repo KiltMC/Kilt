d4e2e3123bbc353d8e38e298fd64672bd20cc4db
Li <9@korewa.li>
2025-10-24 11:22:29 +0000

    feat: using this rather init unnecessary object

--- compat/transfer-api-compat/src/main/kotlin/xyz/bluspring/kilt/compat/transfer/TransferInterop.kt
+++ compat/transfer-api-compat/src/main/kotlin/xyz/bluspring/kilt/compat/transfer/TransferInterop.kt
@@ -26,7 +26,7 @@ import xyz.bluspring.kilt.compat.transfer.item.ForgeSlottedStorage
 
 class TransferInterop : ModInitializer {
     override fun onInitialize() {
-        MinecraftForge.EVENT_BUS.register(TransferInterop())
+        MinecraftForge.EVENT_BUS.register(this)
 
         ItemStorage.SIDED.registerFallback { world, pos, state, blockEntity, direction ->
             if (blockEntity == null)
