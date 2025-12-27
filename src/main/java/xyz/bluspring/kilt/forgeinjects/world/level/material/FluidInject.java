af018cd30cc22dea2489851ff9c96babf5447973
brickmasterhunt <brickmasterlogan@icloud.com>
2025-12-26 21:53:04 -0500

    Bullshit is the fluid registry

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/material/FluidInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/material/FluidInject.java
@@ -1,19 +1,17 @@
 // TRACKED HASH: 8052166e9529780ad90ee8b00eda7d0ee8ffc2ca
 package xyz.bluspring.kilt.forgeinjects.world.level.material;
 
+import io.github.fabricators_of_create.porting_lib.fluids.extensions.FluidExtension;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.extensions.IForgeFluid;
 import net.minecraftforge.fluids.FluidType;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.asm.mixin.Implements;
-import org.spongepowered.asm.mixin.Interface;
-import org.spongepowered.asm.mixin.Intrinsic;
-import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.*;
 import xyz.bluspring.kilt.injections.world.level.material.FluidInjection;
 
-@Mixin(Fluid.class)
-@Implements(@Interface(iface = FluidInjection.class, prefix = "forge$i$"))
+@Mixin(value = Fluid.class)
+@Implements(value = { @Interface(iface = FluidInjection.class, prefix = "forge$i$"), @Interface(iface = FluidExtension.class, prefix = "kilt$porting_lib$")})
 public abstract class FluidInject implements IForgeFluid {
     private FluidType forgeFluidType;
 
@@ -28,5 +26,26 @@ public abstract class FluidInject implements IForgeFluid {
     @Override
     public FluidType forge$getFluidType() {
         return this.forge$i$getFluidType();
+    }
+
+    @Intrinsic
+    public io.github.fabricators_of_create.porting_lib.fluids.FluidType getFluidType() {
+        throw new IllegalStateException();
+    }
+
+    @Unique private boolean kilt$cachedUsePortingLib = false;
+
+    @Intrinsic(displace = true)
+    public io.github.fabricators_of_create.porting_lib.fluids.FluidType kilt$porting_lib$getFluidType() {
+        if (this.kilt$cachedUsePortingLib) {
+            return this.forge$getFluidType();
+        }
+
+        try {
+            return this.forge$getFluidType();
+        } catch (Exception e) {
+            this.kilt$cachedUsePortingLib = true;
+            return this.getFluidType();
+        }
     }
 }
