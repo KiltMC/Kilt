271c463548e57b7cea45ffb0e2e290920a4bb72e
Vorty-SH <252356476+Vorty-SH@users.noreply.github.com>
2026-01-15 08:38:55 +0000

    Add files via upload
    Forces wither storm to think shaders are always running.

---
+++ src/main/java/xyz/bluspring/kilt/mixin/compat/forge/CompatHelperMixin.java
@@ -0,0 +1,26 @@
+package xyz.bluspring.kilt.mixin.compat.forge.crackerslib;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Pseudo
+@Mixin(targets = "nonamecrackers2.crackerslib.common.compat.CompatHelper", remap = false)
+public abstract class CompatHelperMixin {
+
+    private static final Logger LOGGER = LogManager.getLogger("kilt/CompatHelperMixin");
+    private static boolean hasLogged = false; // Track if we've logged yet
+
+    @Inject(method = "areShadersRunning", at = @At("HEAD"), cancellable = true)
+    private static void kilt$forceShaders(CallbackInfoReturnable<Boolean> cir) {
+        if (!hasLogged) {
+            LOGGER.info("[CompatHelperMixin] areShadersRunning() called! Forcing true.");
+            hasLogged = true; // Only log once
+        }
+        cir.setReturnValue(true);
+    }
+}
