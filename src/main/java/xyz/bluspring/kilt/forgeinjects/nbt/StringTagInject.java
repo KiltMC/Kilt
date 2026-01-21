91818d91959df0b1a8080fccf9c42186bb163113
Naz <bluspringindohq@gmail.com>
2026-01-21 20:42:59 +0800

    I do not like the improved NBT accounter system. Fix #622.

--- src/main/java/xyz/bluspring/kilt/forgeinjects/nbt/StringTagInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/nbt/StringTagInject.java
@@ -12,9 +12,9 @@ import xyz.bluspring.kilt.injections.nbt.NbtAccounterInjection;
 public abstract class StringTagInject {
     @Mixin(targets = "net/minecraft/nbt/StringTag$1")
     public static abstract class Inner1Inject {
-        @Redirect(method = "load(Ljava/io/DataInput;ILnet/minecraft/nbt/NbtAccounter;)Lnet/minecraft/nbt/StringTag;", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtAccounter;accountBytes(J)V", ordinal = 1))
-        private void kilt$useImprovedUTFAccounter(NbtAccounter instance, long bytes, @Local String string) {
-            ((NbtAccounterInjection) instance).readUTF(string);
-        }
+//        @Redirect(method = "load(Ljava/io/DataInput;ILnet/minecraft/nbt/NbtAccounter;)Lnet/minecraft/nbt/StringTag;", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtAccounter;accountBytes(J)V", ordinal = 1))
+//        private void kilt$useImprovedUTFAccounter(NbtAccounter instance, long bytes, @Local String string) {
+//            ((NbtAccounterInjection) instance).readUTF(string);
+//        }
     }
 }
