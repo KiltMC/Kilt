b5e60f4a5dc8a3b998dbe97f5db03a5785c46210
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-01-25 13:56:42 +0100

    Set FluidState of wrapped fluid properly.

--- compat/create-compat/src/main/java/xyz/bluspring/kilt/compat/create/mixin/registrate_fabric/FluidAccessor.java
+++ compat/create-compat/src/main/java/xyz/bluspring/kilt/compat/create/mixin/registrate_fabric/FluidAccessor.java
@@ -2,10 +2,13 @@ package xyz.bluspring.kilt.compat.create.mixin.registrate_fabric;
 
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
 import net.minecraft.core.Holder;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @IfModLoaded("registrate-fabric")
 @Mixin(Fluid.class)
@@ -14,5 +17,12 @@ public interface FluidAccessor {
     @Mutable
     @Accessor
     void setBuiltInRegistryHolder(Holder.Reference<Fluid> holder);
+
+    @Invoker
+    void callCreateFluidStateDefinition(StateDefinition.Builder<Fluid, FluidState> builder);
+
+    @Mutable
+    @Accessor
+    void setStateDefinition(StateDefinition<Fluid, FluidState> stateDefinition);
 
 }
