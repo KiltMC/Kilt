ceb113eec54842304a13bb5cad6d883a7587032f
Naz <bluspringindohq@gmail.com>
2026-02-20 03:15:31 +0800

    Fixed #466

--- src/main/java/xyz/bluspring/kilt/forgeinjects/client/multiplayer/MultiPlayerGameModeInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/client/multiplayer/MultiPlayerGameModeInject.java
@@ -1,10 +1,15 @@
 package xyz.bluspring.kilt.forgeinjects.client.multiplayer;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
@@ -17,6 +22,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
@@ -24,12 +30,14 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.Event;
 import org.apache.commons.lang3.mutable.MutableObject;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -121,28 +129,31 @@ public abstract class MultiPlayerGameModeInject {
         return original && !this.destroyingItem.shouldCauseBlockBreakReset(this.minecraft.player.getMainHandItem());
     }
 
-    // Kilt: handled via Architectury
-    /*@Inject(method = "performUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;", ordinal = 0, shift = At.Shift.AFTER), cancellable = true)
-    private void kilt$cancelRightClickEvent(LocalPlayer player, InteractionHand hand, BlockHitResult result, CallbackInfoReturnable<InteractionResult> cir, @Local BlockPos pos, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
-        var event = ForgeHooks.onRightClickBlock(player, hand, pos, result);
-        eventRef.set(event);
+    @Definition(id = "SPECTATOR", field = "Lnet/minecraft/world/level/GameType;SPECTATOR:Lnet/minecraft/world/level/GameType;")
+    @Definition(id = "localPlayerMode", field = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;localPlayerMode:Lnet/minecraft/world/level/GameType;")
+    @Expression("this.localPlayerMode == SPECTATOR")
+    @Inject(method = "performUseItemOn", at = @At("MIXINEXTRAS:EXPRESSION"), cancellable = true)
+    private void kilt$callRightClickBlockEvent(LocalPlayer player, InteractionHand hand, BlockHitResult result, CallbackInfoReturnable<InteractionResult> cir, @Share("event") @NotNull LocalRef<PlayerInteractEvent.RightClickBlock> eventRef, @Local BlockPos pos) {
+        eventRef.set(ForgeHooks.onRightClickBlock(player, hand, pos, result));
 
-        if (event.isCanceled())
-            cir.setReturnValue(event.getCancellationResult());
+        if (eventRef.get().isCanceled())
+            cir.setReturnValue(eventRef.get().getCancellationResult());
     }
 
-    @Inject(method = "performUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;getMainHandItem()Lnet/minecraft/world/item/ItemStack;", ordinal = 0), cancellable = true)
-    private void kilt$checkCanEventRun(LocalPlayer player, InteractionHand hand, BlockHitResult result, CallbackInfoReturnable<InteractionResult> cir, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef, @Local ItemStack stack) {
-        var context = new UseOnContext(player, hand, result);
-
+    @Definition(id = "player", local = @Local(type = LocalPlayer.class, argsOnly = true))
+    @Definition(id = "getMainHandItem", method = "Lnet/minecraft/client/player/LocalPlayer;getMainHandItem()Lnet/minecraft/world/item/ItemStack;")
+    @Definition(id = "isEmpty", method = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z")
+    @Expression("player.getMainHandItem().isEmpty()")
+    @Inject(method = "performUseItemOn", at = @At("MIXINEXTRAS:EXPRESSION"), cancellable = true)
+    private void kilt$tryHandleItemFirstUseEvent(LocalPlayer player, InteractionHand hand, BlockHitResult hitResult, CallbackInfoReturnable<InteractionResult> cir, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef, @Local ItemStack stack) {
+        UseOnContext context = new UseOnContext(player, hand, hitResult);
         if (eventRef.get().getUseItem() != Event.Result.DENY) {
-            var useResult = stack.onItemUseFirst(context);
-
-            if (useResult != InteractionResult.PASS) {
-                cir.setReturnValue(useResult);
+            InteractionResult result = stack.onItemUseFirst(context);
+            if (result != InteractionResult.PASS) {
+                cir.setReturnValue(result);
             }
         }
-    }*/
+    }
 
     @WrapOperation(method = "performUseItemOn", at = {
         @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z", ordinal = 0),
@@ -152,43 +163,35 @@ public abstract class MultiPlayerGameModeInject {
         return original.call(instance) || instance.doesSneakBypassUse(player.level(), pos, player);
     }
 
-    // Kilt: handled via Architectury
-    /*@WrapOperation(method = "performUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;"))
-    private InteractionResult kilt$checkCanUse(BlockState instance, Level level, Player player, InteractionHand interactionHand, BlockHitResult hitResult, Operation<InteractionResult> original, @Local(ordinal = 1) boolean flag, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
-        if (eventRef.get().getUseBlock() == Event.Result.ALLOW || (eventRef.get().getUseBlock() != Event.Result.DENY && !flag)) {
-            original.call(instance, level, player, interactionHand, hitResult);
-        }
-
-        return InteractionResult.PASS;
+    @Definition(id = "bl2", local = @Local(type = boolean.class, ordinal = 1))
+    @Expression("bl2 == 0")
+    @ModifyExpressionValue(method = "performUseItemOn", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean kilt$checkCanUseBlock(boolean original, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+        return eventRef.get().getUseBlock() == Event.Result.ALLOW || (eventRef.get().getUseBlock() != Event.Result.DENY && original);
     }
 
-    @Inject(method = "performUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z", ordinal = 2), cancellable = true)
-    private void kilt$checkShouldDenyEvent(LocalPlayer player, InteractionHand hand, BlockHitResult result, CallbackInfoReturnable<InteractionResult> cir, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+    @Definition(id = "stack", local = @Local(type = ItemStack.class, ordinal = 0))
+    @Definition(id = "isEmpty", method = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z")
+    @Expression("stack.isEmpty() == 0")
+    @ModifyExpressionValue(method = "performUseItemOn", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean kilt$checkCanUseItemOnBlock(boolean original, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+        return eventRef.get().getUseItem() == Event.Result.ALLOW || original;
+    }
+
+    @Definition(id = "player", local = @Local(type = LocalPlayer.class, argsOnly = true))
+    @Definition(id = "getCooldowns", method = "Lnet/minecraft/client/player/LocalPlayer;getCooldowns()Lnet/minecraft/world/item/ItemCooldowns;")
+    @Definition(id = "isOnCooldown", method = "Lnet/minecraft/world/item/ItemCooldowns;isOnCooldown(Lnet/minecraft/world/item/Item;)Z")
+    @Expression("player.getCooldowns().isOnCooldown(?) == 0")
+    @ModifyExpressionValue(method = "performUseItemOn", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean kilt$checkCanUseItemOnBlock2(boolean original, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+        return eventRef.get().getUseItem() == Event.Result.ALLOW || original;
+    }
+
+    @Inject(method = "performUseItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/GameType;isCreative()Z"), cancellable = true)
+    private void kilt$cancelIfDeny(LocalPlayer player, InteractionHand hand, BlockHitResult result, CallbackInfoReturnable<InteractionResult> cir, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
         if (eventRef.get().getUseItem() == Event.Result.DENY)
             cir.setReturnValue(InteractionResult.PASS);
     }
-
-    @ModifyExpressionValue(method = "performUseItemOn", at = {
-        @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z", ordinal = 2),
-        @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemCooldowns;isOnCooldown(Lnet/minecraft/world/item/Item;)Z")
-    })
-    private boolean kilt$checkShouldAllowEvent(boolean original, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
-        if (eventRef.get().getUseItem() == Event.Result.ALLOW)
-            return false;
-
-        return original;
-    }*/
-
-    // Kilt: handled via Architectury
-    /*@Inject(method = "method_41929", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;"), cancellable = true)
-    private void kilt$callItemRightClickEvent(InteractionHand interactionHand, Player player, MutableObject<InteractionResult> mutableObject, int i, CallbackInfoReturnable<Packet> cir, @Local ServerboundUseItemPacket packet) {
-        var cancelResult = ForgeHooks.onItemRightClick(player, interactionHand);
-
-        if (cancelResult != null) {
-            mutableObject.setValue(cancelResult);
-            cir.setReturnValue(packet);
-        }
-    }*/
 
     @Inject(method = "method_41929", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;setItemInHand(Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;)V", shift = At.Shift.AFTER))
     private void kilt$handleForgeDestroyItemEvent(InteractionHand interactionHand, Player player, MutableObject mutableObject, int i, CallbackInfoReturnable<Packet> cir, @Local(ordinal = 0) ItemStack stack, @Local(ordinal = 1) ItemStack stack2) {
--- src/main/java/xyz/bluspring/kilt/forgeinjects/server/level/ServerPlayerGameModeInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/server/level/ServerPlayerGameModeInject.java
@@ -1,24 +1,41 @@
 // TRACKED HASH: 6b790991592a47c6c6a8e8be25116c80e58fd91d
 package xyz.bluspring.kilt.forgeinjects.server.level;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.extensions.IForgeBlock;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.Event;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.bluspring.kilt.util.KiltHelper;
 
 @Mixin(ServerPlayerGameMode.class)
@@ -55,6 +72,67 @@ public abstract class ServerPlayerGameModeInject {
 		}
 
 		return removed;
+	}
+
+	@Definition(id = "gameModeForPlayer", field = "Lnet/minecraft/server/level/ServerPlayerGameMode;gameModeForPlayer:Lnet/minecraft/world/level/GameType;")
+	@Definition(id = "SPECTATOR", field = "Lnet/minecraft/world/level/GameType;SPECTATOR:Lnet/minecraft/world/level/GameType;")
+	@Expression("this.gameModeForPlayer == SPECTATOR")
+	@Inject(method = "useItemOn", at = @At("MIXINEXTRAS:EXPRESSION"), cancellable = true)
+	private void kilt$callRightClickBlockEvent(ServerPlayer player, Level level, ItemStack stack, InteractionHand hand, BlockHitResult hitResult, CallbackInfoReturnable<InteractionResult> cir, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef, @Local BlockPos pos) {
+		eventRef.set(ForgeHooks.onRightClickBlock(player, hand, pos, hitResult));
+
+		if (eventRef.get().isCanceled())
+			cir.setReturnValue(eventRef.get().getCancellationResult());
+	}
+
+	@Definition(id = "player", local = @Local(type = ServerPlayer.class, argsOnly = true))
+	@Definition(id = "getMainHandItem", method = "Lnet/minecraft/server/level/ServerPlayer;getMainHandItem()Lnet/minecraft/world/item/ItemStack;")
+	@Definition(id = "isEmpty", method = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z")
+	@Expression("player.getMainHandItem().isEmpty()")
+	@Inject(method = "useItemOn", at = @At("MIXINEXTRAS:EXPRESSION"), cancellable = true)
+	private void kilt$tryHandleItemFirstUseEvent(ServerPlayer player, Level level, ItemStack stack, InteractionHand hand, BlockHitResult hitResult, CallbackInfoReturnable<InteractionResult> cir, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+		UseOnContext context = new UseOnContext(player, hand, hitResult);
+		if (eventRef.get().getUseItem() != Event.Result.DENY) {
+			InteractionResult result = stack.onItemUseFirst(context);
+			if (result != InteractionResult.PASS) {
+				cir.setReturnValue(result);
+			}
+		}
+	}
+
+	@ModifyVariable(method = "useItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;copy()Lnet/minecraft/world/item/ItemStack;"), ordinal = 1)
+	private boolean kilt$handleSneakBypassUse(boolean original, @Local(argsOnly = true) ServerPlayer player, @Local(argsOnly = true) Level level, @Local BlockPos pos) {
+		return original && !(player.getMainHandItem().doesSneakBypassUse(level, pos, player) && player.getOffhandItem().doesSneakBypassUse(level, pos, player));
+	}
+
+	@Definition(id = "bl2", local = @Local(type = boolean.class, ordinal = 1))
+	@Expression("bl2 == 0")
+	@ModifyExpressionValue(method = "useItemOn", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private boolean kilt$checkCanUseBlock(boolean original, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+		return eventRef.get().getUseBlock() == Event.Result.ALLOW || (eventRef.get().getUseBlock() != Event.Result.DENY && original);
+	}
+
+	@Definition(id = "stack", local = @Local(type = ItemStack.class, ordinal = 0, argsOnly = true))
+	@Definition(id = "isEmpty", method = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z")
+	@Expression("stack.isEmpty() == 0")
+	@ModifyExpressionValue(method = "useItemOn", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private boolean kilt$checkCanUseItemOnBlock(boolean original, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+		return eventRef.get().getUseItem() == Event.Result.ALLOW || original;
+	}
+
+	@Definition(id = "player", local = @Local(type = ServerPlayer.class, argsOnly = true))
+	@Definition(id = "getCooldowns", method = "Lnet/minecraft/server/level/ServerPlayer;getCooldowns()Lnet/minecraft/world/item/ItemCooldowns;")
+	@Definition(id = "isOnCooldown", method = "Lnet/minecraft/world/item/ItemCooldowns;isOnCooldown(Lnet/minecraft/world/item/Item;)Z")
+	@Expression("player.getCooldowns().isOnCooldown(?) == 0")
+	@ModifyExpressionValue(method = "useItemOn", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private boolean kilt$checkCanUseItemOnBlock2(boolean original, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+		return eventRef.get().getUseItem() == Event.Result.ALLOW || original;
+	}
+
+	@Inject(method = "useItemOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayerGameMode;isCreative()Z"), cancellable = true)
+	private void kilt$cancelIfDeny(ServerPlayer player, Level level, ItemStack stack, InteractionHand hand, BlockHitResult hitResult, CallbackInfoReturnable<InteractionResult> cir, @Share("event") LocalRef<PlayerInteractEvent.RightClickBlock> eventRef) {
+		if (eventRef.get().getUseItem() == Event.Result.DENY)
+			cir.setReturnValue(InteractionResult.PASS);
 	}
 
 	/*@WrapOperation(
