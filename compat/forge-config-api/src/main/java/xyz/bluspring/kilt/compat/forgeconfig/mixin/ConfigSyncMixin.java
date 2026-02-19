c0d810a9516df702e9caebc68c0f13fde7897461
Naz <bluspringindohq@gmail.com>
2026-02-20 01:59:31 +0800

    GO AWAY

---
+++ compat/forge-config-api/src/main/java/xyz/bluspring/kilt/compat/forgeconfig/mixin/ConfigSyncMixin.java
@@ -0,0 +1,25 @@
+package xyz.bluspring.kilt.compat.forgeconfig.mixin;
+
+import com.mojang.datafixers.util.Pair;
+import com.moulberry.mixinconstraints.annotations.IfModLoaded;
+import fuzs.forgeconfigapiport.impl.network.config.ConfigSync;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.Collections;
+import java.util.List;
+
+@IfModLoaded("forgeconfigapiport")
+@Mixin(ConfigSync.class)
+public abstract class ConfigSyncMixin {
+    /**
+     * @author BluSpring
+     * @reason Prevent Forge Config API Port from handling things.
+     */
+    @Overwrite
+    public static void unloadSyncedConfig() {
+    }
+}
