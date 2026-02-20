b62ba28f1c3aab304ef14d21304415d433e59058
Naz <bluspringindohq@gmail.com>
2026-02-20 17:18:02 +0800

    fucking explode

---
+++ src/main/java/xyz/bluspring/kilt/mixin/workarounds/ServerConfigurationPacketListenerMixin.java
@@ -0,0 +1,19 @@
+package xyz.bluspring.kilt.mixin.workarounds;
+
+import net.minecraft.network.protocol.configuration.ServerConfigurationPacketListener;
+import net.minecraft.server.network.ConfigurationTask;
+import net.neoforged.neoforge.common.extensions.IServerConfigurationPacketListenerExtension;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Intrinsic;
+import org.spongepowered.asm.mixin.Mixin;
+
+
+@Implements(@Interface(iface = IServerConfigurationPacketListenerExtension.class, prefix = "kilt$i$"))
+@Mixin(ServerConfigurationPacketListener.class)
+public interface ServerConfigurationPacketListenerMixin extends IServerConfigurationPacketListenerExtension {
+    @Intrinsic
+    default void kilt$i$finishCurrentTask(ConfigurationTask.Type task) {
+        this.finishCurrentTask(task);
+    }
+}
