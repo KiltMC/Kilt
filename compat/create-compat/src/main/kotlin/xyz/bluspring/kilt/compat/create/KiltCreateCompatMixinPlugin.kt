ce7f28835631b34c76d77a99f325ccca64872dee
BluSpring <bluspringindohq@gmail.com>
2026-02-23 19:18:54 +0800

    Fix crash with MapRenderer on Create Forge

--- compat/create-compat/src/main/kotlin/xyz/bluspring/kilt/compat/create/KiltCreateCompatMixinPlugin.kt
+++ compat/create-compat/src/main/kotlin/xyz/bluspring/kilt/compat/create/KiltCreateCompatMixinPlugin.kt
@@ -25,6 +25,10 @@ class KiltCreateCompatMixinPlugin : IMixinConfigPlugin {
             return FabricLoader.getInstance().isModLoaded("create") && !KiltLoader.instance.hasMod("create") && MixinConstraints.shouldApplyMixin(mixinClassName)
         }
 
+        if (packageName == "create_forge") {
+            return KiltLoader.instance.hasMod("create") && MixinConstraints.shouldApplyMixin(mixinClassName)
+        }
+
         return MixinConstraints.shouldApplyMixin(mixinClassName)
     }
 
