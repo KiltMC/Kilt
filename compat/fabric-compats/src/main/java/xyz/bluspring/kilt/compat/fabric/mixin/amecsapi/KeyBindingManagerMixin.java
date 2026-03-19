24860ed410680fde6dce4bd9342395f6262c3efc
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-03-20 00:41:54 +0100

    Get rid of the "default" keybinding layer in Amecs. (#705)

--- compat/fabric-compats/src/main/java/xyz/bluspring/kilt/compat/fabric/mixin/amecsapi/KeyBindingManagerMixin.java
+++ compat/fabric-compats/src/main/java/xyz/bluspring/kilt/compat/fabric/mixin/amecsapi/KeyBindingManagerMixin.java
@@ -1,33 +1,41 @@
 package xyz.bluspring.kilt.compat.fabric.mixin.amecsapi;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.blaze3d.platform.InputConstants;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
-import de.siphalor.amecs.api.KeyBindingUtils;
-import de.siphalor.amecs.impl.AmecsAPI;
 import de.siphalor.amecs.impl.KeyBindingManager;
 import net.minecraft.client.KeyMapping;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.Map;
 
 @Pseudo
 @IfModLoaded(value = "amecsapi", maxVersion = "1.7.0")
 @Mixin(KeyBindingManager.class)
 public abstract class KeyBindingManagerMixin {
-    @IfModLoaded(value = "amecsapi", minVersion = "1.5.3")
-    @Inject(method = "areExactModifiersPressed", at = @At("HEAD"), cancellable = true, require = 0)
-    private static void kilt$amecsapi$disableIfNoModifiersAssigned(KeyMapping keyBinding, CallbackInfoReturnable<Boolean> cir) {
-        if (KeyBindingUtils.getBoundModifiers(keyBinding).isUnset() || AmecsAPI.CURRENT_MODIFIERS.isUnset())
-            cir.setReturnValue(false);
-    }
 
-    @IfModLoaded(value = "amecsapi", maxVersion = "1.5.1")
-    @ModifyExpressionValue(method = "getMatchingKeyBindings", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;"), require = 0)
-    private static <T> Stream<T> kilt$amecsapi$removeIfNoModifiersAssigned(Stream<T> original) {
-        return original.filter(keyBinding -> !KeyBindingUtils.getBoundModifiers((KeyMapping) keyBinding).isUnset() && !AmecsAPI.CURRENT_MODIFIERS.isUnset());
+    @Redirect(
+            method = "register",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lde/siphalor/amecs/impl/KeyBindingManager;addKeyBindingToListFromMap(Ljava/util/Map;Lnet/minecraft/client/KeyMapping;)Z"
+            ),
+            slice = @Slice(
+                    from = @At(
+                            value = "FIELD",
+                            target = "Lde/siphalor/amecs/impl/KeyBindingManager;keysById:Ljava/util/Map;"
+                    )
+            )
+    )
+    private static boolean kilt$skipRegularKeybind(
+            Map<InputConstants.Key, List<KeyMapping>> targetMap, KeyMapping keyBinding
+    ) {
+        return false;
     }
 }
