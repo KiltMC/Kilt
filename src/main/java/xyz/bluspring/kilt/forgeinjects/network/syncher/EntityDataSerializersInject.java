21fd30d91d1df43a2542673949d85fc00bffffbe
BluSpring <bluspringindohq@gmail.com>
2026-03-25 21:09:32 +0800

    Improve mod compatibility of entity serializers (fixes KiltMC/Kilt#704)

--- src/main/java/xyz/bluspring/kilt/forgeinjects/network/syncher/EntityDataSerializersInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/network/syncher/EntityDataSerializersInject.java
@@ -4,25 +4,32 @@ package xyz.bluspring.kilt.forgeinjects.network.syncher;
 import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.util.CrudeIncrementalIntIdentityHashBiMap;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraftforge.common.ForgeHooks;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EntityDataSerializers.class)
 public class EntityDataSerializersInject {
-    @Shadow @Final private static CrudeIncrementalIntIdentityHashBiMap<EntityDataSerializer<?>> SERIALIZERS;
+    @ModifyReturnValue(method = "getSerializer", at = @At("RETURN"))
+    private static EntityDataSerializer<?> kilt$getSerializerFromForge(EntityDataSerializer<?> original, @Local(argsOnly = true) int id) {
+        if (original == null) {
+            return ForgeHooks.kilt$getSerializer(id);
+        }
 
-    @Inject(method = "getSerializer", at = @At("HEAD"), cancellable = true)
-    private static void kilt$getSerializerFromForge(int id, CallbackInfoReturnable<EntityDataSerializer<?>> cir) {
-        cir.setReturnValue(ForgeHooks.getSerializer(id, SERIALIZERS));
+        return original;
     }
 
-    @Inject(method = "getSerializedId", at = @At("HEAD"), cancellable = true)
-    private static void kilt$getSerializerIdFromForge(EntityDataSerializer<?> serializer, CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(ForgeHooks.getSerializerId(serializer, SERIALIZERS));
+    @ModifyReturnValue(method = "getSerializedId", at = @At("RETURN"))
+    private static int kilt$getSerializerIdFromForge(int original, @Local(argsOnly = true) EntityDataSerializer<?> serializer) {
+        if (original == -1) {
+            return ForgeHooks.kilt$getSerializerId(serializer);
+        }
+
+        return original;
     }
 }
