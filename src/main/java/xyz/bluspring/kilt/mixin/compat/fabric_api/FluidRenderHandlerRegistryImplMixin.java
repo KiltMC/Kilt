cf3ffae3f7a51b2409c4e1ed6d607356112b03f8
BluSpring <bluspringindohq@gmail.com>
2026-03-25 22:44:20 +0800

    Fixed incredibly elegant empty fluid rendering on Fabric (fixes KiltMC/Kilt#506)

--- src/main/java/xyz/bluspring/kilt/mixin/compat/fabric_api/FluidRenderHandlerRegistryImplMixin.java
+++ src/main/java/xyz/bluspring/kilt/mixin/compat/fabric_api/FluidRenderHandlerRegistryImplMixin.java
@@ -5,6 +5,8 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
 import net.fabricmc.fabric.impl.client.rendering.fluid.FluidRenderHandlerRegistryImpl;
 import net.minecraft.world.level.material.Fluid;
+
+import net.minecraftforge.common.ForgeMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import xyz.bluspring.kilt.workarounds.FluidHandlerWorkaround;
@@ -17,6 +19,9 @@ public class FluidRenderHandlerRegistryImplMixin {
             return original;
 
         var fluidType = fluid.forge$getFluidType();
+        if (fluidType == ForgeMod.EMPTY_TYPE.get())
+            return null;
+
         return FluidHandlerWorkaround.INSTANCE.getFluidRenderHandler(fluidType);
     }
 
@@ -26,6 +31,9 @@ public class FluidRenderHandlerRegistryImplMixin {
             return original;
 
         var fluidType = fluid.forge$getFluidType();
+        if (fluidType == ForgeMod.EMPTY_TYPE.get())
+            return null;
+
         return FluidHandlerWorkaround.INSTANCE.getFluidRenderHandler(fluidType);
     }
 }
