05b4588968ff016cddb13797799634b18efd2064
BluSpring <bluspringindohq@gmail.com>
2026-04-08 21:16:29 +0800

    Fix add entity spamming logs

--- src/main/java/xyz/bluspring/kilt/injects/world/level/entity/PersistentEntitySectionManagerInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/world/level/entity/PersistentEntitySectionManagerInject.java
@@ -1,11 +1,9 @@
 package xyz.bluspring.kilt.injects.world.level.entity;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalLongRef;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.entity.EntityAccess;
-import net.minecraft.world.level.entity.EntitySection;
-import net.minecraft.world.level.entity.PersistentEntitySectionManager;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +15,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.bluspring.kilt.injections.world.level.entity.PersistentEntitySectionManagerInjection;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.entity.EntityAccess;
+import net.minecraft.world.level.entity.EntitySection;
+import net.minecraft.world.level.entity.PersistentEntitySectionManager;
 
 @Mixin(PersistentEntitySectionManager.class)
 public abstract class PersistentEntitySectionManagerInject<T extends EntityAccess> implements PersistentEntitySectionManagerInjection<T> {
@@ -37,7 +38,7 @@ public abstract class PersistentEntitySectionManagerInject<T extends EntityAcces
 
     @Inject(method = "addEntity", at = @At("HEAD"), cancellable = true)
     private void kilt$callEntityJoinLevelEvent(T entity, boolean worldGenSpawned, CallbackInfoReturnable<Boolean> cir) {
-        if (!this.kilt$callWithoutEvent.getAndSet(false) && entity instanceof Entity e && NeoForge.EVENT_BUS.post(new EntityJoinLevelEvent(e, e.level(), worldGenSpawned)).isCanceled())
+        if (this.kilt$callWithoutEvent.getAndSet(false) && entity instanceof Entity e && NeoForge.EVENT_BUS.post(new EntityJoinLevelEvent(e, e.level(), worldGenSpawned)).isCanceled())
             cir.setReturnValue(false);
     }
 
