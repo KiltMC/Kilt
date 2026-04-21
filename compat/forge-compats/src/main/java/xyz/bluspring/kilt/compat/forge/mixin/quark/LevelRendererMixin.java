d43c2c8ccd824d10d491e32c3fd72926a11312af
BluSpring <bluspringindohq@gmail.com>
2026-04-22 07:35:50 +0800

    Update mods for compat in dev

--- compat/forge-compats/src/main/java/xyz/bluspring/kilt/compat/forge/mixin/quark/LevelRendererMixin.java
+++ compat/forge-compats/src/main/java/xyz/bluspring/kilt/compat/forge/mixin/quark/LevelRendererMixin.java
@@ -1,15 +1,8 @@
 package xyz.bluspring.kilt.compat.forge.mixin.quark;
 
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.violetmoon.quark.base.item.QuarkMusicDiscItem;
-import org.violetmoon.quark.content.tools.module.AmbientDiscsModule;
+
+import net.minecraft.client.renderer.LevelRenderer;
 
 @Mixin(value = LevelRenderer.class, priority = 1051)
 public class LevelRendererMixin {
