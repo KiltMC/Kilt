5d061b507b8de36c0eb440d4697ec2af677ce90b
BluSpring <bluspringindohq@gmail.com>
2026-04-21 16:41:18 +0800

    oops

--- compat/create-compat/src/main/kotlin/xyz/bluspring/kilt/compat/create/KiltCreateCompatPreLaunch.kt
+++ compat/create-compat/src/main/kotlin/xyz/bluspring/kilt/compat/create/KiltCreateCompatPreLaunch.kt
@@ -9,7 +9,7 @@ import xyz.bluspring.knit.loader.KnitLoader
 
 class KiltCreateCompatPreLaunch : PreLaunchEntrypoint {
     override fun onPreLaunch() {
-        if (FabricLoader.getInstance().environmentType == EnvType.CLIENT && Kilt.loader.hasMod("flywheel")) {
+        if (FabricLoader.getInstance().environmentType == EnvType.CLIENT && Kilt.loader.hasMod("flywheel") && !FabricLoader.getInstance().isModLoaded("flywheel")) {
             val KILT_ERROR_MESSAGE = "Kilt: Failed to start Kilt, please read the exception below!"
             KnitLoader.instance.displayErrorGUI(KILT_ERROR_MESSAGE, DependencyException("Detected Flywheel NeoForge, please use either Create Fabric or install the Vanillin mod to use Flywheel Fabric!"))
             return
