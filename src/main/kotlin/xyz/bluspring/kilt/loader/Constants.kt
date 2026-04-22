d4460a3c169fe99f82a2c5ee197af3abfefe8b65
BluSpring <bluspringindohq@gmail.com>
2026-04-22 18:14:36 +0800

    Update upstream

--- src/main/kotlin/xyz/bluspring/kilt/loader/Constants.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/Constants.kt
@@ -4,7 +4,7 @@ import org.apache.maven.artifact.versioning.DefaultArtifactVersion
 
 object Constants {
     @JvmField val NEOFORGE_LOADER_VERSION = DefaultArtifactVersion("4") // 1.21.1
-    @JvmField val NEOFORGE_API_VERSION = DefaultArtifactVersion("21.1.224")
+    @JvmField val NEOFORGE_API_VERSION = DefaultArtifactVersion("21.1.227")
 
     // Kotlin for Forge version. We're trying to emulate KFF.
     @JvmField val KFF_VERSION = DefaultArtifactVersion("5.8.0")
