0429800e5e5561c0608b0510a44b0b3a3850f80e
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-05-01 13:39:12 +0200

    Additional registrate fix.

--- compat/create-compat/src/main/java/xyz/bluspring/kilt/compat/create/mixin/registrate_fabric/FluidEntryMixin.java
+++ compat/create-compat/src/main/java/xyz/bluspring/kilt/compat/create/mixin/registrate_fabric/FluidEntryMixin.java
@@ -1,22 +1,43 @@
 package xyz.bluspring.kilt.compat.create.mixin.registrate_fabric;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
 import com.tterrag.registrate.AbstractRegistrate;
 import com.tterrag.registrate.fabric.RegistryObject;
 import com.tterrag.registrate.util.entry.FluidEntry;
 import com.tterrag.registrate.util.entry.RegistryEntry;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
 import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.bluspring.kilt.Kilt;
+
+import java.util.Optional;
 
 @IfModLoaded("registrate-fabric")
 @SuppressWarnings("rawtypes")
 @Mixin(FluidEntry.class)
 public abstract class FluidEntryMixin extends RegistryEntry {
+
+    @Unique
+    private boolean kilt$isForge = false;
+
     public FluidEntryMixin(AbstractRegistrate<?> owner, RegistryObject delegate) {
         super(owner, delegate);
+    }
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    public void kilt$init(AbstractRegistrate owner, RegistryObject delegate, CallbackInfo ci) {
+        if (Kilt.Companion.getLoader().hasMod(owner.getModid())) {
+            kilt$isForge = true;
+        }
     }
 
     public FluidType getType() {
@@ -26,5 +47,13 @@ public abstract class FluidEntryMixin extends RegistryEntry {
     @Intrinsic
     public ForgeFlowingFluid getSource() {
         return (ForgeFlowingFluid) ((ForgeFlowingFluid) get()).getSource();
+    }
+
+    @WrapMethod(method = "getBucket")
+    public <I extends Item> Optional<I> kilt$getBucket(Operation<Optional<I>> original) {
+        if (kilt$isForge) {
+            return Optional.ofNullable((I) ((ForgeFlowingFluid) get()).getBucket());
+        }
+        return original.call();
     }
 }
