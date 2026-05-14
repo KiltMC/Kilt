ca86e4aef6026e035fdd703911ac3645bfe68378
Cart <81428538+Cartrigger@users.noreply.github.com>
2026-05-14 00:49:53 -0400

    Change mixin priority + or - 1 from 1000 to something else (#760)

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/material/FluidInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/material/FluidInject.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.*;
 import xyz.bluspring.kilt.injections.world.level.material.FluidInjection;
 
 // We use a lower priority to make sure porting lib injects first, so create and other fabric mods inject into that one.
-@Mixin(value = Fluid.class, priority = 1001)
+@Mixin(value = Fluid.class, priority = 1050)
 @Implements(value = { @Interface(iface = FluidInjection.class, prefix = "forge$i$"), @Interface(iface = FluidExtension.class, prefix = "kilt$porting_lib$")})
 public abstract class FluidInject implements IForgeFluid {
     private FluidType forgeFluidType;
