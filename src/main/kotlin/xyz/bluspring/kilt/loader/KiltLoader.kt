6441ea3cf932b91f6b25f1714dc75f6a40e833c6
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-05-18 21:11:28 +0200

    Use none ordering as a fallback for dependency override.

--- src/main/kotlin/xyz/bluspring/kilt/loader/KiltLoader.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/KiltLoader.kt
@@ -379,7 +379,7 @@ class KiltLoader : KnitModLoader<ForgeMod>(Kilt.MOD_ID, "Forge") {
                 }
                 for (dep in dependencyOverrides) {
                     val prevDep = modifiedDependencies[dep.key]
-                    val additionalData = prevDep?.additionalData ?: mapOf()
+                    val additionalData = prevDep?.additionalData ?: mapOf("ordering" to IModInfo.Ordering.NONE)
                     modifiedDependencies[dep.key] = ModDependency(
                         id = prevDep?.id ?: dep.key,
                         constraint = dep.value.version.map { ForgeVersionConstraint(it) as VersionConstraint }.orElse(prevDep?.constraint ?: ForgeVersionConstraint(VersionRange.createFromVersionSpec("(,1.0],[1.0,)"))),
