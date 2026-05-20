1535956ef562d357391a45280eecbba5f9baec7e
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-05-20 03:06:37 +0200

    Fix seeds not placeable on rich soil in Farmer's Delight Refabricated. (#774)
    * Fix seeds not placeable on rich soil in Farmer's Delight Refabricated.
    
    * Only use `canSustainPlant` when explicitly overridden.
    
    * No need to specify priority explicitly.

--- src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/block/BushBlockInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/world/level/block/BushBlockInject.java
@@ -1,18 +1,19 @@
 // TRACKED HASH: d335200a23b4a1e738aa9a7ea4e390f5f72a6c5d
 package xyz.bluspring.kilt.forgeinjects.world.level.block;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.IPlantable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xyz.bluspring.kilt.util.KiltHelper;
 
 @Mixin(BushBlock.class)
 public abstract class BushBlockInject extends Block implements IPlantable {
@@ -20,11 +21,24 @@ public abstract class BushBlockInject extends Block implements IPlantable {
         super(properties);
     }
 
-    @Inject(method = "canSurvive", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/BushBlock;mayPlaceOn(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z"), cancellable = true)
-    private void kilt$checkCanSustain(BlockState state, LevelReader level, BlockPos pos, CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 1) BlockPos pos2) {
-        if (state.getBlock() == this) {
-            cir.setReturnValue(level.getBlockState(pos2).canSustainPlant(level, pos2, Direction.UP, this));
+    @WrapOperation(
+            method = "canSurvive",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/BushBlock;mayPlaceOn(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z")
+    )
+    private boolean kilt$checkCanSustain(
+            BushBlock instance, BlockState stateBelow, BlockGetter level, BlockPos posBelow, Operation<Boolean> original,
+            @Local(argsOnly = true) BlockState state
+    ) {
+        if (
+                state.getBlock() == this &&
+                KiltHelper.INSTANCE.hasMethodOverride(
+                        stateBelow.getBlock().getClass(), Block.class, "canSustainPlant",
+                        BlockState.class, BlockGetter.class, BlockPos.class, Direction.class, IPlantable.class
+                )
+        ) {
+            return stateBelow.canSustainPlant(level, posBelow, Direction.UP, this);
         }
+        return original.call(instance, stateBelow, level, posBelow);
     }
 
     // Kilt: handled by Porting Lib
