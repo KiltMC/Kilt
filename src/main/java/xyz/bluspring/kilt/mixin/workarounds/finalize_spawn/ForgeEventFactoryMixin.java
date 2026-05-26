a6602b8147d67b134e69312f893bcd1bd40f7950
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-05-26 12:53:07 +0200

    ForgeEventFactory compat patch

---
+++ src/main/java/xyz/bluspring/kilt/mixin/workarounds/finalize_spawn/ForgeEventFactoryMixin.java
@@ -0,0 +1,39 @@
+package xyz.bluspring.kilt.mixin.workarounds.finalize_spawn;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraftforge.event.ForgeEventFactory;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = ForgeEventFactory.class, priority = 950)
+public class ForgeEventFactoryMixin {
+
+    @Shadow
+    @Final
+    private static ThreadLocal<Operation<SpawnGroupData>> kilt$fabricOriginal;
+
+    @WrapOperation(
+            method = "onFinalizeSpawn",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/Mob;finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/MobSpawnType;Lnet/minecraft/world/entity/SpawnGroupData;Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/world/entity/SpawnGroupData;"
+            )
+    )
+    private static SpawnGroupData kilt$onFinalizeSpawn(Mob instance, ServerLevelAccessor serverLevelAccessor, DifficultyInstance difficultyInstance, MobSpawnType mobSpawnType, SpawnGroupData spawnGroupData, CompoundTag compoundTag, Operation<SpawnGroupData> forgeOriginal) {
+        var actualOriginal = kilt$fabricOriginal.get();
+        if (actualOriginal == null) {
+            actualOriginal = forgeOriginal;
+        }
+        return actualOriginal.call(instance, serverLevelAccessor, difficultyInstance, mobSpawnType, spawnGroupData, compoundTag);
+    }
+
+}
