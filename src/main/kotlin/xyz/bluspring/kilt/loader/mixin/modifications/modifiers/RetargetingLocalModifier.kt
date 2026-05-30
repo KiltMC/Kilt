462604ee4f258e05b65532e0b50ee46e5969f3f6
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-05-30 16:25:33 +0200

    Style improvement

--- src/main/kotlin/xyz/bluspring/kilt/loader/mixin/modifications/modifiers/RetargetingLocalModifier.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/mixin/modifications/modifiers/RetargetingLocalModifier.kt
@@ -141,8 +141,8 @@ data class RetargetingLocalModifier(
                     val type = values["type"]
 
                     val mapping = paramToLocalMapping.filter {
-                            it.key.descriptor == (if (it.key.unboxRef) unboxRefDescriptor(descriptor, signature).descriptor else descriptor.descriptor)
-                            && (matches(ordinal, it.key.local.ordinal, -1)
+                            it.key.descriptor == (if (it.key.unboxRef) unboxRefDescriptor(descriptor, signature) else descriptor).descriptor
+                                && (matches(ordinal, it.key.local.ordinal, -1)
                             && matches(print, it.key.local.print, false)
                             && matches(index, it.key.local.index, -1)
                             && matches(name, it.key.local.name.toList(), listOf<String>())
