b1730669d848ce1106ec753ae1ba973b55201ce8
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-05-31 19:56:31 +0200

    Fix `EntityJoinLevelEvent` never being fired for non-player entities on the server side (and being fired twice for players).

--- src/main/java/xyz/bluspring/kilt/injects/world/level/entity/PersistentEntitySectionManagerInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/world/level/entity/PersistentEntitySectionManagerInject.java
@@ -38,7 +38,7 @@ public abstract class PersistentEntitySectionManagerInject<T extends EntityAcces
 
     @Inject(method = "addEntity", at = @At("HEAD"), cancellable = true)
     private void kilt$callEntityJoinLevelEvent(T entity, boolean worldGenSpawned, CallbackInfoReturnable<Boolean> cir) {
-        if (this.kilt$callWithoutEvent.getAndSet(false) && entity instanceof Entity e && NeoForge.EVENT_BUS.post(new EntityJoinLevelEvent(e, e.level(), worldGenSpawned)).isCanceled())
+        if (!this.kilt$callWithoutEvent.getAndSet(false) && entity instanceof Entity e && NeoForge.EVENT_BUS.post(new EntityJoinLevelEvent(e, e.level(), worldGenSpawned)).isCanceled())
             cir.setReturnValue(false);
     }
 
