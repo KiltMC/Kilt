db8666cbab7744d39755c73d790cea144d1a75ed
BluSpring <bluspringindohq@gmail.com>
2026-06-03 21:15:53 +0800

    Fixed like 90% of the Aeronautics bugs in 6 lines, I just reorganized things.

--- src/main/java/xyz/bluspring/kilt/injects/world/item/ItemStackInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/world/item/ItemStackInject.java
@@ -1,29 +1,18 @@
 // TRACKED HASH: debf6874a4415fcbb527c106a281c5bd27a0b454
 package xyz.bluspring.kilt.injects.world.item;
 
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
 import com.bawnorton.mixinsquared.TargetHandler;
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.core.Holder;
-import net.minecraft.core.component.DataComponentType;
-import net.minecraft.core.component.PatchedDataComponentMap;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.EquipmentSlotGroup;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.ItemAttributeModifiers;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.item.enchantment.ItemEnchantments;
-import net.minecraft.world.level.ItemLike;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.neoforged.neoforge.common.CommonHooks;
 import net.neoforged.neoforge.common.ItemAbility;
 import net.neoforged.neoforge.common.MutableDataComponentHolder;
@@ -40,8 +29,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.bluspring.kilt.injections.world.item.ItemStackInjection;
 import xyz.bluspring.kilt.util.KiltHelper;
 
-import java.util.function.BiConsumer;
-import java.util.function.Function;
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.component.PatchedDataComponentMap;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
+import net.minecraft.world.level.ItemLike;
 
 @Mixin(value = ItemStack.class, priority = 1050)
 public abstract class ItemStackInject implements MutableDataComponentHolder, IItemStackExtension, ItemStackInjection {
@@ -54,11 +55,56 @@ public abstract class ItemStackInject implements MutableDataComponentHolder, IIt
     @Shadow
     public abstract ItemEnchantments getEnchantments();
 
+    @Shadow
+    public abstract boolean is(Item item);
+
     public ItemStackInject(ItemLike item, int count) {}
 
     @Override
     public boolean isComponentsPatchEmpty() {
         return !this.isEmpty() ? this.components.isPatchEmpty() : true;
+    }
+
+    @ModifyReturnValue(method = "is(Lnet/minecraft/core/Holder;)Z", at = @At("RETURN"))
+    private boolean kilt$checkMatchesDeferredHolder(boolean original, @Local(argsOnly = true) Holder<Item> holder) {
+        return original || this.is(holder.value());
+    }
+
+    @Unique
+    private Function<UseOnContext, InteractionResult> kilt$callback;
+
+    @Inject(method = "useOn", at = @At("HEAD"), cancellable = true)
+    private void kilt$tryPlaceItemInWorld(UseOnContext context, CallbackInfoReturnable<InteractionResult> cir) {
+        if (this.kilt$callback == null) {
+            var e = NeoForge.EVENT_BUS.post(new UseItemOnBlockEvent(context, UseItemOnBlockEvent.UsePhase.ITEM_AFTER_BLOCK));
+            if (e.isCanceled()) {
+                cir.setReturnValue(e.getCancellationResult().result());
+            } else if (!context.getLevel().isClientSide()) {
+                cir.setReturnValue(CommonHooks.onPlaceItemIntoWorld(context));
+            }
+        }
+    }
+
+    @Override
+    public InteractionResult onItemUseFirst(UseOnContext context) {
+        this.kilt$callback = c -> this.getItem().onItemUseFirst((ItemStack) (Object) this, c);
+        var e = NeoForge.EVENT_BUS.post(new UseItemOnBlockEvent(context, UseItemOnBlockEvent.UsePhase.ITEM_BEFORE_BLOCK));
+        if (e.isCanceled()) return e.getCancellationResult().result();
+        var result = this.useOn(context);
+        this.kilt$callback = null;
+        return result;
+    }
+
+    @WrapOperation(method = "useOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;"))
+    private InteractionResult kilt$tryUseOnCallback(Item instance, UseOnContext context, Operation<InteractionResult> original) {
+        if (kilt$callback != null) {
+            var value = kilt$callback.apply(context);
+            kilt$callback = null;
+
+            return value;
+        }
+
+        return original.call(instance, context);
     }
 
     @WrapOperation(method = "getMaxStackSize", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getOrDefault(Lnet/minecraft/core/component/DataComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
@@ -88,13 +134,14 @@ public abstract class ItemStackInject implements MutableDataComponentHolder, IIt
         return original;
     }
 
-    @WrapWithCondition(method = "setDamageValue", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;set(Lnet/minecraft/core/component/DataComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
-    private <T> boolean trySetItemDamage(ItemStack instance, DataComponentType<? super T> component, T value) {
+    @WrapOperation(method = "setDamageValue", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;set(Lnet/minecraft/core/component/DataComponentType;Ljava/lang/Object;)Ljava/lang/Object;"))
+    private <T> Object trySetItemDamage(ItemStack instance, DataComponentType<? super T> component, T value, Operation<Object> original) {
         if (KiltHelper.INSTANCE.hasMethodOverride(getItem().getClass(), Item.class, "setDamage", ItemStack.class, int.class)) {
             getItem().setDamage((ItemStack) (Object) this, (Integer) value);
-            return false;
+            return null;
         }
-        return true;
+
+        return original.call(instance, component, value);
     }
 
     @ModifyReturnValue(method = "getMaxDamage", at = @At("RETURN"))
@@ -115,43 +162,6 @@ public abstract class ItemStackInject implements MutableDataComponentHolder, IIt
     @Override
     public ItemEnchantments getTagEnchantments() {
         return this.getEnchantments();
-    }
-
-    @Unique
-    private Function<UseOnContext, InteractionResult> kilt$callback;
-
-    @Inject(method = "useOn", at = @At("HEAD"), cancellable = true)
-    private void kilt$tryPlaceItemInWorld(UseOnContext context, CallbackInfoReturnable<InteractionResult> cir) {
-        if (!context.getLevel().isClientSide() && this.kilt$callback == null) {
-            var e = NeoForge.EVENT_BUS.post(new UseItemOnBlockEvent(context, UseItemOnBlockEvent.UsePhase.ITEM_AFTER_BLOCK));
-            if (e.isCanceled()) {
-                cir.setReturnValue(e.getCancellationResult().result());
-            } else {
-                cir.setReturnValue(CommonHooks.onPlaceItemIntoWorld(context)); // TODO: this isn't good
-            }
-        }
-    }
-
-    @Override
-    public InteractionResult onItemUseFirst(UseOnContext context) {
-        this.kilt$callback = c -> this.getItem().onItemUseFirst((ItemStack) (Object) this, c);
-        var e = NeoForge.EVENT_BUS.post(new UseItemOnBlockEvent(context, UseItemOnBlockEvent.UsePhase.ITEM_BEFORE_BLOCK));
-        if (e.isCanceled()) return e.getCancellationResult().result();
-        var result = this.useOn(context);
-        this.kilt$callback = null;
-        return result;
-    }
-
-    @WrapOperation(method = "useOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;useOn(Lnet/minecraft/world/item/context/UseOnContext;)Lnet/minecraft/world/InteractionResult;"))
-    private InteractionResult kilt$tryUseOnCallback(Item instance, UseOnContext context, Operation<InteractionResult> original) {
-        if (kilt$callback != null) {
-            var value = kilt$callback.apply(context);
-            kilt$callback = null;
-
-            return value;
-        }
-
-        return original.call(instance, context);
     }
 
     @ModifyReceiver(method = "forEachModifier(Lnet/minecraft/world/entity/EquipmentSlotGroup;Ljava/util/function/BiConsumer;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/component/ItemAttributeModifiers;forEach(Lnet/minecraft/world/entity/EquipmentSlotGroup;Ljava/util/function/BiConsumer;)V", ordinal = 0))
