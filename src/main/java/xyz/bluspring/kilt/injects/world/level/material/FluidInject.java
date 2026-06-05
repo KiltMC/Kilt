4a10fd67c9d1585b11a2652373eddfb96c50979a
BluSpring <bluspringindohq@gmail.com>
2026-06-06 00:07:18 +0800

    Try to fix more fluid type bullshit

--- src/main/java/xyz/bluspring/kilt/injects/world/level/material/FluidInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/world/level/material/FluidInject.java
@@ -1,16 +1,18 @@
 // TRACKED HASH: 8052166e9529780ad90ee8b00eda7d0ee8ffc2ca
 package xyz.bluspring.kilt.injects.world.level.material;
 
-import net.minecraft.world.level.material.Fluid;
 import net.neoforged.neoforge.common.CommonHooks;
 import net.neoforged.neoforge.common.extensions.IFluidExtension;
 import net.neoforged.neoforge.fluids.FluidType;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Implements;
 import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
 import xyz.bluspring.kilt.util.KiltHelper;
 import xyz.bluspring.kilt.workarounds.FluidWorkaround;
+
+import net.minecraft.world.level.material.Fluid;
 
 @Implements(@Interface(iface = FluidWorkaround.class, prefix = "kilt$i$"))
 @Mixin(Fluid.class)
@@ -31,6 +33,7 @@ public abstract class FluidInject implements IFluidExtension, FluidWorkaround {
         return forgeFluidType;
     }
 
+    @Intrinsic
     public FluidType kilt$i$getFluidType() {
         return this.neo$getFluidType();
     }
--- src/main/java/xyz/bluspring/kilt/injects/world/level/material/FluidStateInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/world/level/material/FluidStateInject.java
@@ -1,10 +1,21 @@
 // TRACKED HASH: da4c7d69266cd226e6baab431327d61c6d9516d3
 package xyz.bluspring.kilt.injects.world.level.material;
 
-import net.minecraft.world.level.material.FluidState;
 import net.neoforged.neoforge.common.extensions.IFluidStateExtension;
+import net.neoforged.neoforge.fluids.FluidType;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
+import xyz.bluspring.kilt.workarounds.FluidWorkaround;
 
+import net.minecraft.world.level.material.FluidState;
+
+@Implements(@Interface(iface = FluidWorkaround.class, prefix = "kilt$i$"))
 @Mixin(FluidState.class)
 public abstract class FluidStateInject implements IFluidStateExtension {
+    @Intrinsic
+    public FluidType kilt$i$getFluidType() {
+        return this.neo$getFluidType();
+    }
 }
