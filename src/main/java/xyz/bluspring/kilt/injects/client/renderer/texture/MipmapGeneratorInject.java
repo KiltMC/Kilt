977db75651b518b6f0f4ccaf501b4b71cf73def8
BluSpring <bluspringindohq@gmail.com>
2026-06-08 02:30:35 +0800

    Heavily improve mod compatibility of mipmap fixes

--- src/main/java/xyz/bluspring/kilt/injects/client/renderer/texture/MipmapGeneratorInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/client/renderer/texture/MipmapGeneratorInject.java
@@ -1,60 +1,44 @@
 // TRACKED HASH: ca60bbd06c7c885c4461c9ed6a53102032e79acf
 package xyz.bluspring.kilt.injects.client.renderer.texture;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import com.mojang.blaze3d.platform.NativeImage;
-import net.minecraft.client.renderer.texture.MipmapGenerator;
 import net.neoforged.neoforge.client.ClientHooks;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-@Mixin(MipmapGenerator.class)
+import net.minecraft.client.renderer.texture.MipmapGenerator;
+
+@Mixin(value = MipmapGenerator.class, priority = 1050)
 public abstract class MipmapGeneratorInject {
-    @Shadow private static boolean hasTransparentPixel(NativeImage image) {
-        throw new IllegalStateException();
-    };
-    @Shadow private static int alphaBlend(int col0, int col1, int col2, int col3, boolean transparent) {
-        throw new IllegalStateException();
-    };
-
-
-    /**
-     * @author BluSpring
-     * @reason no other way to add a continue, other than writing a fuckin' mixin plugin for it, or doing some weird workarounds.
-     */
-    @Overwrite
-    public static NativeImage[] generateMipLevels(NativeImage[] images, int mipLevel) {
-        if (mipLevel + 1 <= images.length) {
-            return images;
-        } else {
-            NativeImage[] nativeImages = new NativeImage[mipLevel + 1];
-            nativeImages[0] = images[0];
-            boolean bl = hasTransparentPixel(nativeImages[0]);
-
-            int maxMipLevel = ClientHooks.getMaxMipmapLevel(nativeImages[0].getWidth(), nativeImages[0].getHeight());
-            for(int i = 1; i <= mipLevel; ++i) {
-                if (i < images.length) {
-                    nativeImages[i] = images[i];
-                } else {
-                    NativeImage nativeImage = nativeImages[i - 1];
-                    NativeImage nativeImage2 = new NativeImage(Math.max(1, nativeImage.getWidth() >> 1), Math.max(1, nativeImage.getHeight() >> 1), false);;
-                    if (i <= maxMipLevel) {
-                        int j = nativeImage2.getWidth();
-                        int k = nativeImage2.getHeight();
-
-                        for (int l = 0; l < j; ++l) {
-                            for (int m = 0; m < k; ++m) {
-                                nativeImage2.setPixelRGBA(l, m, alphaBlend(nativeImage.getPixelRGBA(l * 2 + 0, m * 2 + 0), nativeImage.getPixelRGBA(l * 2 + 1, m * 2 + 0), nativeImage.getPixelRGBA(l * 2 + 0, m * 2 + 1), nativeImage.getPixelRGBA(l * 2 + 1, m * 2 + 1), bl));
-                            }
-                        }
-                    }
-
-                    nativeImages[i] = nativeImage2;
-                }
-            }
-
-            return nativeImages;
-        }
+    @Inject(method = "generateMipLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/MipmapGenerator;hasTransparentPixel(Lcom/mojang/blaze3d/platform/NativeImage;)Z", shift = At.Shift.AFTER))
+    private static void kilt$getMaxMipLevel(NativeImage[] images, int mipLevel, CallbackInfoReturnable<NativeImage[]> cir, @Local(ordinal = 1) NativeImage[] nativeImages, @Share("maxMipLevel") LocalIntRef maxMipLevel) {
+        maxMipLevel.set(ClientHooks.getMaxMipmapLevel(nativeImages[0].getWidth(), nativeImages[0].getHeight()));
     }
 
+    @ModifyArgs(method = "generateMipLevels", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/NativeImage;<init>(IIZ)V"))
+    private static void kilt$guardFromInvalidTexSize(Args args) {
+        args.set(0, Math.max(1, args.get(0)));
+        args.set(1, Math.max(1, args.get(1)));
+    }
+
+    @Definition(id = "l", local = @Local(type = int.class, ordinal = 4))
+    @Definition(id = "j", local = @Local(type = int.class, ordinal = 2))
+    @Expression("l < j")
+    @ModifyExpressionValue(method = "generateMipLevels", at = @At("MIXINEXTRAS:EXPRESSION"), require = 0, expect = 0)
+    private static boolean kilt$avoidBlendingPixelsIfInvalidMip(boolean original, @Local(ordinal = 1) int currentMipLevel, @Share("maxMipLevel") LocalIntRef maxMipLevel) {
+        if (currentMipLevel > maxMipLevel.get())
+            return false;
+
+        return original;
+    }
 }
--- src/main/java/xyz/bluspring/kilt/injects/client/renderer/texture/SpriteLoaderInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/client/renderer/texture/SpriteLoaderInject.java
@@ -1,32 +1,21 @@
 // TRACKED HASH: 38ed9a92a3865ea4db1c28fa04a9e858ee73d618
 package xyz.bluspring.kilt.injects.client.renderer.texture;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.platform.NativeImage;
-import net.minecraft.client.renderer.texture.SpriteContents;
-import net.minecraft.client.renderer.texture.SpriteLoader;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.metadata.animation.AnimationMetadataSection;
-import net.minecraft.client.resources.metadata.animation.FrameSize;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
-import net.neoforged.neoforge.client.ClientHooks;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Map;
+import net.minecraft.client.renderer.texture.SpriteLoader;
+import net.minecraft.resources.ResourceLocation;
 
 @Mixin(SpriteLoader.class)
 public abstract class SpriteLoaderInject {
     @Shadow @Final private ResourceLocation location;
 
-    @Redirect(method = "stitch", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;log2(I)I", ordinal = 2))
+    @Redirect(method = "stitch", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;log2(I)I", ordinal = 2), require = 0, expect = 0)
     private int kilt$avoidMipmapLowering(int value, @Local(argsOnly = true) int mipLevel) {
         return mipLevel;
     }
