1b2f64f48d34c67dd759ac176fa31cba77d17e99
BluSpring <bluspringindohq@gmail.com>
2026-06-07 21:27:44 +0800

    Fix weird registry crashes

--- src/main/java/xyz/bluspring/kilt/injects/core/RegistryInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/core/RegistryInject.java
@@ -3,13 +3,17 @@ package xyz.bluspring.kilt.injects.core;
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Cancellable;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
+import com.mojang.serialization.DataResult;
 import net.neoforged.neoforge.common.extensions.IHolderExtension;
 import net.neoforged.neoforge.registries.IRegistryExtension;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 
 @Mixin(Registry.class)
 public interface RegistryInject<T> extends IRegistryExtension<T> {
@@ -17,13 +21,18 @@ public interface RegistryInject<T> extends IRegistryExtension<T> {
     @Definition(id = "Reference", type = Holder.Reference.class)
     @Expression("@(value) instanceof Reference")
     @ModifyExpressionValue(method = "safeCastToReference", at = @At("MIXINEXTRAS:EXPRESSION"))
-    private Holder<T> kilt$tryUseForgeDelegate(Holder<T> original) {
+    private Holder<T> kilt$tryUseForgeDelegate(Holder<T> original, @Cancellable CallbackInfoReturnable<DataResult<Holder.Reference<T>>> cir) {
         var delegate = ((IHolderExtension<T>) original).getDelegate();
 
         //noinspection RedundantIfStatement
         if (original == delegate)
             return original;
 
-        return delegate;
+        if (delegate instanceof Holder.Reference<T> reference) {
+            cir.setReturnValue(DataResult.success(reference));
+            return reference;
+        }
+
+        return original;
     }
 }
