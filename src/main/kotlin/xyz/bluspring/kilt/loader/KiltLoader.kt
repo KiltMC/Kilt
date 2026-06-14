990c00c6d3ddad0ef38ec6f6939d65b7d7635280
BluSpring <bluspringindohq@gmail.com>
2026-06-14 21:07:14 +0800

    Fix mods not actually loading on the dedicated server

--- src/main/kotlin/xyz/bluspring/kilt/loader/KiltLoader.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/KiltLoader.kt
@@ -547,10 +547,10 @@ class KiltLoader : KnitModLoader<NeoForgeMod>(Kilt.MOD_ID, "NeoForge") {
             // Make sure the environment is valid for these bridges before applying them.
             if (entry.environment != ModEnvironment.BOTH) {
                 if (FabricLoader.getInstance().environmentType == EnvType.CLIENT && entry.environment != ModEnvironment.CLIENT)
-                    return
+                    continue
 
                 if (FabricLoader.getInstance().environmentType == EnvType.SERVER && entry.environment != ModEnvironment.SERVER)
-                    return
+                    continue
             }
 
             try {
