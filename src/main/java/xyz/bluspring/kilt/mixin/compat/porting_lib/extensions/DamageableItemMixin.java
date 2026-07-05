ab54aa4020b059cfcbd6964cb9b16a28c3268619
BluSpring <bluspringindohq@gmail.com>
2026-07-05 18:48:02 +0800

    mixin to all of Porting Lib's extensions

--- src/main/java/xyz/bluspring/kilt/mixin/compat/porting_lib/extensions/DamageableItemMixin.java
+++
@@ -1,9 +0,0 @@
-package xyz.bluspring.kilt.mixin.compat.porting_lib.extensions;
-
-import io.github.fabricators_of_create.porting_lib.item.DamageableItem;
-import net.neoforged.neoforge.common.extensions.IItemExtension;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(DamageableItem.class)
-public interface DamageableItemMixin extends IItemExtension {
-}
