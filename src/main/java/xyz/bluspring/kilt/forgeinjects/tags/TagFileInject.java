fe00a209f4ece046bd6a29a93374fa506531dcc3
BluSpring <bluspringindohq@gmail.com>
2026-07-17 22:21:04 +0800

    Fix incompatibility with Fabric API

--- src/main/java/xyz/bluspring/kilt/forgeinjects/tags/TagFileInject.java
+++ src/main/java/xyz/bluspring/kilt/forgeinjects/tags/TagFileInject.java
@@ -14,8 +14,11 @@ import xyz.bluspring.kilt.injections.tags.TagFileInjection;
 
 import java.util.List;
 
+import net.fabricmc.fabric.api.tag.v1.FabricTagFile;
+import net.fabricmc.fabric.impl.tag.TagFileHooks;
+
 @Mixin(TagFile.class)
-public abstract class TagFileInject implements TagFileInjection {
+public abstract class TagFileInject implements TagFileInjection, FabricTagFile, TagFileHooks {
     @ModifyReturnValue(method = "method_43950", at = @At("RETURN"))
     private static App<RecordCodecBuilder.Mu<TagFile>, TagFile> kilt$appendRemoveTags(App<RecordCodecBuilder.Mu<TagFile>, TagFile> original, @Local(argsOnly = true) RecordCodecBuilder.Instance<TagFile> instance) {
         return instance.group(original,
@@ -29,22 +32,23 @@ public abstract class TagFileInject implements TagFileInjection {
             });
     }
 
-    @Unique private List<TagEntry> remove = List.of();
-
-    public List<TagEntry> remove() {
-        return remove;
-    }
+    // implemented by Fabric API
+//    @Unique private List<TagEntry> remove = List.of();
+//
+//    public List<TagEntry> remove() {
+//        return remove;
+//    }
 
     public TagFileInject(List<TagEntry> entries, boolean replace) {}
 
     @CreateInitializer
     public TagFileInject(List<TagEntry> entries, boolean replace, List<TagEntry> remove) {
         this(entries, replace);
-        this.remove = remove;
+        this.kilt$setRemove(remove);
     }
 
     @Override
     public void kilt$setRemove(List<TagEntry> remove) {
-        this.remove = remove;
+        this.fabric_setRemove(remove);
     }
 }
--- src/main/java/xyz/bluspring/kilt/injections/tags/TagFileInjection.java
+++ src/main/java/xyz/bluspring/kilt/injections/tags/TagFileInjection.java
@@ -4,7 +4,9 @@ import net.minecraft.tags.TagEntry;
 
 import java.util.List;
 
-public interface TagFileInjection {
+import net.fabricmc.fabric.api.tag.v1.FabricTagFile;
+
+public interface TagFileInjection extends FabricTagFile {
     List<TagEntry> remove();
     void kilt$setRemove(List<TagEntry> remove);
 }
