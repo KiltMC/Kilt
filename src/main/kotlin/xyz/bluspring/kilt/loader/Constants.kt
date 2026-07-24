f253e615de7d5f497b6907ff4c5b46b1cbca3955
BluSpring <bluspringindohq@gmail.com>
2026-07-24 21:19:29 +0800

    apparently some people use this check, which is funny because sometimes this doesn't even work in Neo

--- src/main/kotlin/xyz/bluspring/kilt/loader/Constants.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/Constants.kt
@@ -3,6 +3,6 @@ package xyz.bluspring.kilt.loader
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion
 
 object Constants {
-    @JvmField val NEOFORGE_LOADER_VERSION = DefaultArtifactVersion("4") // 1.21.1
+    @JvmField val NEOFORGE_LOADER_VERSION = DefaultArtifactVersion("4.0.43") // 1.21.1
     @JvmField val NEOFORGE_API_VERSION = DefaultArtifactVersion("21.1.238")
 }
