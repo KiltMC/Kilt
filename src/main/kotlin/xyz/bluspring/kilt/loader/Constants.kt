5ae938c94c55c2a1be32cb43e4e062b51a5add70
BluSpring <bluspringindohq@gmail.com>
2026-07-31 21:34:55 +0800

    update Forge API to 47.4.22

--- src/main/kotlin/xyz/bluspring/kilt/loader/Constants.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/Constants.kt
@@ -4,7 +4,7 @@ import org.apache.maven.artifact.versioning.DefaultArtifactVersion
 
 object Constants {
     @JvmField val FORGE_LOADER_VERSION = DefaultArtifactVersion("47") // 1.20.1
-    @JvmField val FORGE_API_VERSION = DefaultArtifactVersion("47.4.10")
+    @JvmField val FORGE_API_VERSION = DefaultArtifactVersion("47.4.22")
 
     // Kotlin for Forge version. We're trying to emulate KFF.
     @JvmField val KFF_VERSION = DefaultArtifactVersion("4.12.0")
