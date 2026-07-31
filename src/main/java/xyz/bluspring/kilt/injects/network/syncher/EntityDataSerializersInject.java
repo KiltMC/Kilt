0eba66e1b7a2973b6b4a3eb1de736222609194a7
BluSpring <bluspringindohq@gmail.com>
2026-07-31 23:40:50 +0800

    try to deal with mod incompatibility issues with EntityDataSerializersInject

--- src/main/java/xyz/bluspring/kilt/injects/network/syncher/EntityDataSerializersInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/network/syncher/EntityDataSerializersInject.java
@@ -1,28 +1,29 @@
 // TRACKED HASH: 381478d70082864904d99c0e2af6d7b72e1615b7
 package xyz.bluspring.kilt.injects.network.syncher;
 
-import net.minecraft.network.syncher.EntityDataSerializer;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.util.CrudeIncrementalIntIdentityHashBiMap;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.neoforged.neoforge.common.CommonHooks;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.network.syncher.EntityDataSerializer;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.util.CrudeIncrementalIntIdentityHashBiMap;
 
 @Mixin(EntityDataSerializers.class)
 public class EntityDataSerializersInject {
     @Shadow @Final private static CrudeIncrementalIntIdentityHashBiMap<EntityDataSerializer<?>> SERIALIZERS;
 
-    @Inject(method = "getSerializer", at = @At("HEAD"), cancellable = true)
-    private static void kilt$getSerializerFromForge(int id, CallbackInfoReturnable<EntityDataSerializer<?>> cir) {
-        cir.setReturnValue(CommonHooks.getSerializer(id, SERIALIZERS));
+    @WrapOperation(method = "getSerializer", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CrudeIncrementalIntIdentityHashBiMap;byId(I)Ljava/lang/Object;"))
+    private static <V> V kilt$getSerializerFromForge(CrudeIncrementalIntIdentityHashBiMap<V> instance, int id, Operation<V> original) {
+        return (V) CommonHooks.kilt$getSerializer(id, () -> (EntityDataSerializer<?>) original.call(instance, id));
     }
 
-    @Inject(method = "getSerializedId", at = @At("HEAD"), cancellable = true)
-    private static void kilt$getSerializerIdFromForge(EntityDataSerializer<?> serializer, CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(CommonHooks.getSerializerId(serializer, SERIALIZERS));
+    @WrapOperation(method = "getSerializedId", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/CrudeIncrementalIntIdentityHashBiMap;getId(Ljava/lang/Object;)I"))
+    private static <V> int kilt$getSerializerIdFromForge(CrudeIncrementalIntIdentityHashBiMap<V> instance, V value, Operation<Integer> original) {
+        return CommonHooks.kilt$getSerializerId((EntityDataSerializer<?>) value, () -> original.call(instance, value));
     }
 }
