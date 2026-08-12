f1aad1449d63b2e8916a7933f6987b706d152621
BluSpring <bluspringindohq@gmail.com>
2026-08-12 17:00:19 +0800

    try to improve mod compatibility of FluidInteractionRegistry

--- src/main/java/xyz/bluspring/kilt/injects/world/level/block/LiquidBlockInject.java
+++ src/main/java/xyz/bluspring/kilt/injects/world/level/block/LiquidBlockInject.java
@@ -1,28 +1,25 @@
 // TRACKED HASH: 9d42bfbad9755a8542e0412d358b0c5c2d30ee69
 package xyz.bluspring.kilt.injects.world.level.block;
 
-import com.google.common.collect.Lists;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.neoforged.neoforge.fluids.FluidInteractionRegistry;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Intrinsic;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import xyz.bluspring.kilt.injections.world.level.block.LiquidBlockInjection;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LiquidBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FlowingFluid;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.FluidState;
-import net.neoforged.neoforge.fluids.FluidInteractionRegistry;
-import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import xyz.bluspring.kilt.helpers.mixin.CreateInitializer;
-import xyz.bluspring.kilt.injections.world.level.block.LiquidBlockInjection;
-
-import java.util.List;
-import java.util.function.Supplier;
 
 @Mixin(value = LiquidBlock.class, priority = 1070)
 @Implements(@Interface(iface = LiquidBlockInjection.class, prefix = "kilt$i$"))
@@ -34,14 +31,12 @@ public abstract class LiquidBlockInject extends Block {
         super(properties);
     }
 
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/LiquidBlock;shouldSpreadLiquid(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"), method = "neighborChanged")
-    public boolean kilt$useForgeNeighborsChanged(LiquidBlock instance, Level level, BlockPos blockPos, BlockState blockState) {
-        return !FluidInteractionRegistry.canInteract(level, blockPos);
-    }
+    @WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/LiquidBlock;shouldSpreadLiquid(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"), method = {"neighborChanged", "onPlace"})
+    public boolean kilt$checkHandleNeo(LiquidBlock instance, Level level, BlockPos pos, BlockState state, Operation<Boolean> original) {
+        if (FluidInteractionRegistry.kilt$canInteract(level, pos, false))
+            return false;
 
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/LiquidBlock;shouldSpreadLiquid(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"), method = "onPlace")
-    public boolean kilt$useForgeOnPlace(LiquidBlock instance, Level level, BlockPos blockPos, BlockState blockState) {
-        return !FluidInteractionRegistry.canInteract(level, blockPos);
+        return original.call(instance, level, pos, state);
     }
 
     @Intrinsic
