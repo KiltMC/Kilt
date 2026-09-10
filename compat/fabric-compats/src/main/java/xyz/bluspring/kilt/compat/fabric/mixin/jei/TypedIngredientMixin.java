9e8f0d67411f10234bf0d864bd151a6dbcef14b4
Acuadragon100 <30689683+Acuadragon100@users.noreply.github.com>
2026-09-11 01:40:13 +0200

    Update TypedIngredientMixin to support the latest version of JEI. (#1002)

--- compat/fabric-compats/src/main/java/xyz/bluspring/kilt/compat/fabric/mixin/jei/TypedIngredientMixin.java
+++ compat/fabric-compats/src/main/java/xyz/bluspring/kilt/compat/fabric/mixin/jei/TypedIngredientMixin.java
@@ -4,13 +4,14 @@ import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
 import mezz.jei.api.fabric.ingredients.fluids.IJeiFluidIngredient;
-import mezz.jei.library.ingredients.TypedIngredient;
 import net.neoforged.neoforge.fluids.FluidStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import xyz.bluspring.kilt.util.neoforge.fluid.FluidTransferUtils;
 
+@Pseudo
 @IfModLoaded("jei")
-@Mixin(TypedIngredient.class)
+@Mixin(targets = {"mezz.jei.common.ingredients.TypedIngredient", "mezz.jei.library.ingredients.TypedIngredient"})
 public class TypedIngredientMixin<T> {
 
     @WrapMethod(method = "getIngredient", remap = false)
