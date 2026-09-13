20a752722bcf9690ac6cf9762616a5da458296aa
BluSpring <bluspringindohq@gmail.com>
2026-09-13 09:51:38 +0800

    Fix #1005

--- compat/forge-compats/src/main/java/xyz/bluspring/kilt/compat/forge/mixin/cyclopscore/ModBaseMixin.java
+++ compat/forge-compats/src/main/java/xyz/bluspring/kilt/compat/forge/mixin/cyclopscore/ModBaseMixin.java
@@ -2,19 +2,20 @@ package xyz.bluspring.kilt.compat.forge.mixin.cyclopscore;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
-import net.minecraft.core.registries.Registries;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 
+import net.minecraft.core.registries.Registries;
+
 @IfModLoaded("cyclopscore")
 @Pseudo
 @Mixin(targets = "org.cyclops.cyclopscore.init.ModBase")
 public class ModBaseMixin {
     @Dynamic
-    @ModifyExpressionValue(method = "beforeRegistriedFilled", at = @At(value = "INVOKE", target = "Lnet/minecraft/resources/ResourceKey;equals(Ljava/lang/Object;)Z"))
+    @ModifyExpressionValue(method = "beforeRegistriedFilled", at = @At(value = "INVOKE", target = "Ljava/lang/Object;equals(Ljava/lang/Object;)Z"))
     private boolean kilt$fixDifferentRegistryOrder(boolean original, RegisterEvent event) {
         return event.getRegistryKey().equals(Registries.PARTICLE_TYPE);
     }
