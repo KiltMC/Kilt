2f98a62df5bb0525acdf502925fd01c7ed8d2f7a
BluSpring <bluspringindohq@gmail.com>
2026-09-15 15:14:31 +0800

    Update NeoForge

--- src/main/kotlin/xyz/bluspring/kilt/loader/Constants.kt
+++ src/main/kotlin/xyz/bluspring/kilt/loader/Constants.kt
@@ -3,5 +3,5 @@ package xyz.bluspring.kilt.loader
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion
 
 object Constants {
-    @JvmField val NEOFORGE_API_VERSION = DefaultArtifactVersion("26.1.2.97")
+    @JvmField val NEOFORGE_API_VERSION = DefaultArtifactVersion("26.1.2.109")
 }
